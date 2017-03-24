c9cf125a86b8b0f2a0fcc6135b2bafd969ef6e6b
Nick Sutterer <apotonick@gmail.com>
2017-03-24 17:10:55 +0800

    simplify nested tracing. always pass a new stack in to a call, it will get disposed of if it's not nested automatically.

--- lib/trailblazer/circuit/trace.rb
+++ lib/trailblazer/circuit/trace.rb
@@ -6,9 +6,9 @@ module Trailblazer
         activity_name, is_nested = circuit.instance_variable_get(:@name)[activity]
         activity_name ||= activity
 
-        Run.(activity, direction, args, stack: is_nested ? [] : stack, **flow_options).tap do |direction, outgoing_options, **flow_options|
+        Run.(activity, direction, args, stack: [], **flow_options).tap do |direction, outgoing_options, **flow_options| # TODO: USE KW ARG FOR :stack
           # TODO: fix the inspect, we need a snapshot, deep-nested.
-          stack << [activity_name, activity, direction, outgoing_options, outgoing_options.inspect, is_nested ? flow_options[:stack] : nil ]
+          stack << [activity_name, activity, direction, outgoing_options, outgoing_options.inspect, flow_options[:stack].any? ? flow_options[:stack] : nil ]
         end
       end
 
