2c12abd4885af4ce7468c76b77f02154e496b259
Nick Sutterer <apotonick@gmail.com>
2017-03-29 16:34:41 +0800

    more tests for tracing.

--- test/docs/activity_test.rb
+++ test/docs/activity_test.rb
@@ -47,5 +47,45 @@ class DocsActivityTest < Minitest::Spec
 
     direction.inspect.must_equal "#<End: default {}>"
     options.must_equal({:author=>"Nick", :content=>"World peace!"})
+
+    # ---
+    #- tracing
+
+    #:trace-act
+    require "trailblazer/circuit/present"
+
+    activity = Circuit::Activity({id: "Blog/Publish",
+      Blog::Write=>"Blog::Write",Blog::SpellCheck=>"Blog::SpellCheck",Blog::Correct=>"Blog::Correct", Blog::Publish=>"Blog::Publish" }) { |evt|
+      {
+        evt[:Start]      => { Circuit::Right => Blog::Write },
+        Blog::Write      => { Circuit::Right => Blog::SpellCheck },
+        Blog::SpellCheck => { Circuit::Right => Blog::Publish, Circuit::Left => Blog::Correct },
+        Blog::Correct    => { Circuit::Right => Blog::SpellCheck },
+        Blog::Publish    => { Circuit::Right => evt[:End] }
+      }
+    }
+    #:trace-act end
+
+    #:trace-call
+    stack=[]
+
+    direction, options, flow = activity.(
+      activity[:Start],
+      { author: "Nick" },
+      runner: Trailblazer::Circuit::Trace.new, stack: stack
+    )
+    #:trace-call end
+
+    Circuit::Trace::Present.tree(stack)
+=begin
+  #:trace-res
+  Circuit::Trace::Present.tree(stack)
+   |--> #<Start: default {}>{:author=>"Nick"}
+   |--> Blog::Write{:author=>"Nick", :content=>"World peace!"}
+   |--> Blog::SpellCheck{:author=>"Nick", :content=>"World peace!"}
+   |--> Blog::Publish{:author=>"Nick", :content=>"World peace!"}
+   `--> #<End: default {}>{:author=>"Nick", :content=>"World peace!"}
+  #:trace-res end
+=end
   end
 end
