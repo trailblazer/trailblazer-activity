79af52fe9891b19815976de87aa3d755cea0c1d1
Nick Sutterer <apotonick@gmail.com>
2017-04-07 20:30:46 +0800

    extract activity.rb.

--- lib/trailblazer/circuit.rb
+++ lib/trailblazer/circuit.rb
@@ -126,11 +126,6 @@ module Trailblazer
     class Left < Right
     end
 
-    def self.Activity(*args)
-      Activity.new(*args)
-    end
-
-
     # DSL
     #   events[:Start]
     #
@@ -145,51 +140,10 @@ module Trailblazer
 
       evts.new(*events.values)
     end
-
-    # This is a code structure to encapsulate the circuit execution behavior and the
-    # start, intermediate and end events, within a "physical" business process.
-    #
-    #   activity[:Start]
-    #   activity.()
-    #   activity.values
-    Activity = Struct.new(:circuit, :events) do
-      def [](*args)
-        events[*args]
-      end
-
-      def call(*args, &block)
-        circuit.(*args, &block)
-      end
-    end
-
-    # DSL
-    #  Conveniently build an Activity with Circuit instance and
-    # all its signals/events.
-    def self.Activity(name=:default, events={}, end_events=nil, implementation=false, &block)
-      # default events:
-      start   = events[:start] || { default: Start.new(:default) }
-      _end    = events[:end]   || { default: End.new(:default) }
-
-      events = { start: start }.merge(events)
-      events = { end:   _end  }.merge(events)
-
-      end_events ||= _end.values
-
-      evts = Circuit::Events(events)
-      circuit = Circuit(name, evts, end_events, &block)
-
-      # DISCUSS: remove this!
-      circuit = implementation.(circuit) if implementation
-
-      Activity.new(circuit, evts)
-    end
-
-    def self.Circuit(name=:default, events, end_events)
-      Circuit.new(yield(events), end_events, name)
-    end
   end
 end
 
+require "trailblazer/circuit/activity"
 require "trailblazer/circuit/task"
 require "trailblazer/circuit/alter"
 require "trailblazer/circuit/trace"
