1a1c34ac4ecb8f27dbcdc9aa406706e2c35b513e
Nick Sutterer <apotonick@gmail.com>
2017-04-27 14:50:39 +0200

    allow ruby 2.0 in Trace.

--- lib/trailblazer/circuit/trace.rb
+++ lib/trailblazer/circuit/trace.rb
@@ -4,11 +4,11 @@ module Trailblazer
 
     # Every `activity.call` is considered nested
     class Trace
-      def call(activity, direction, args, debug:, stack:, **flow_options)
+      def call(activity, direction, args, debug:raise, stack:raise, **flow_options)
         activity_name, _ = debug[activity]
         activity_name ||= activity
 
-        Run.(activity, direction, args, stack: [], **flow_options).tap do |direction, outgoing_options, **flow_options| # TODO: USE KW ARG FOR :stack
+        Run.(activity, direction, args, stack:[], **flow_options).tap do |direction, outgoing_options, **flow_options| # TODO: USE KW ARG FOR :stack
           # raise activity_name.inspect if flow_options[:stack].nil? # TODO: remove this.
           # TODO: fix the inspect, we need a snapshot, deep-nested.
           stack << [activity_name, activity, direction, outgoing_options, outgoing_options.inspect, flow_options[:stack].any? ? flow_options[:stack] : nil ]
