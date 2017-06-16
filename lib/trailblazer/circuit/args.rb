7f5b6adecce6bd8de05eea68ca2c331810a92a82
Nick Sutterer <apotonick@gmail.com>
2017-06-16 09:38:56 +0200

    it's Trailblazer::Args now, without any coupling to flows.

--- lib/trailblazer/circuit/args.rb
+++ lib/trailblazer/circuit/args.rb
@@ -1,53 +1,57 @@
 module Trailblazer
-  class Circuit
-    class Args
-      class << self
-        # Returns a {Proc} that, when called, invokes the `proc` argument with keyword arguments.
-        # This is known as "step (call) interface".
-        #
-        # This is commonly used by `Operation::step` to wrap the argument and make it
-        # callable in the circuit.
-        #
-        #   my_proc = ->(options, **kws) { options["i got called"] = true }
-        #   task    = Trailblazer::Circuit::Args::KW(my_proc)
-        #   task.(options = {})
-        #   options["i got called"] #=> true
-        #
-        # Alternatively, you can pass a symbol and an `:exec_context`.
-        #
-        #   my_proc = :some_method
-        #   task    = Trailblazer::Circuit::Args::KW(my_proc)
-        #
-        #   class A
-        #     def some_method(options, **kws)
-        #       options["i got called"] = true
-        #     end
-        #   end
-        #
-        #   task.(options = {}, exec_context: A.new)
-        #   options["i got called"] #=> true
-        def KW(proc)
-          if proc.is_a? Symbol
-            ->(*args) { meth!(proc, *args) } # Task interface
-          else
-            ->(*args) { call!(proc, *args) } # Task interface
-          end
+  # @note This might go to trailblazer-args along with `Context` at some point.
+  class Args
+    class << self
+      # Returns a {Proc} that, when called, invokes the `proc` argument with keyword arguments.
+      # This is known as "step (call) interface".
+      #
+      # This is commonly used by `Operation::step` to wrap the argument and make it
+      # callable in the circuit.
+      #
+      #   my_proc = ->(options, **kws) { options["i got called"] = true }
+      #   task    = Trailblazer::Args::KW(my_proc)
+      #   task.(options = {})
+      #   options["i got called"] #=> true
+      #
+      # Alternatively, you can pass a symbol and an `:exec_context`.
+      #
+      #   my_proc = :some_method
+      #   task    = Trailblazer::Args::KW(my_proc)
+      #
+      #   class A
+      #     def some_method(options, **kws)
+      #       options["i got called"] = true
+      #     end
+      #   end
+      #
+      #   task.(options = {}, exec_context: A.new)
+      #   options["i got called"] #=> true
+      def KW(proc)
+        if proc.is_a? Symbol
+          ->(*args) { call_method!(proc, *args) }
+        else
+          ->(*args) { call_callable!(proc,   *args) }
         end
+      end
 
-        private
+      private
 
-        # Calls `proc` with a "step" interface.
-        def call!(proc, options, flow_options)
-          proc.(options, **options.to_hash)
-        end
+      # Calls `proc` with a "step" interface.
+      # Override this for your own step strategy.
+      def call!(proc, options)
+        proc.(options, **options.to_hash) # Step interface: (options, **)
+      end
 
-        # Make the context's instance method a "lambda" and reuse #call!.
-        # TODO: should we make :context a kwarg?
-        def meth!(proc, options, flow_options)
-          call!(flow_options[:exec_context].method(proc), options, flow_options)
-        end
+      # Note that both #call_callable! and #call_method! drop most of the args.
+      # If you need those, override this class.
+      def call_callable!(proc, options, *)
+        call!(proc, options)
+      end
+
+      # Make the context's instance method a "lambda" and reuse #call!.
+      def call_method!(proc, options, exec_context:raise, **)
+        call!(exec_context.method(proc), options)
       end
     end
   end
-
 end
