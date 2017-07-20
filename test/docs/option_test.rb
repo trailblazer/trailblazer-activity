d9cb37dad8119e6bcfd78af5bd61029dca1c2d39
Nick Sutterer <apotonick@gmail.com>
2017-07-20 11:09:50 +0200

    test callable. cleanup.

--- test/docs/option_test.rb
+++ test/docs/option_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class OptionTest < Minitest::Spec
+  def assert_result(result)
+    result.must_equal( [{:a=>1}, 2, {:b=>3}] )
+
+      positional.inspect.must_equal %{{:a=>1}}
+      keywords.inspect.must_equal %{{:a=>2, :b=>3}}
+  end
+
   describe "positional and kws" do
     class Step
       def with_positional_and_keywords(options, a:nil, **more_options)
@@ -12,10 +19,16 @@ class OptionTest < Minitest::Spec
       [ options, a, more_options ]
     end
 
+    class WithPositionalAndKeywords
+      def self.call(options, a:nil, **more_options)
+        [ options, a, more_options ]
+      end
+    end
+
     let(:positional) { { a: 1 } }
     let(:keywords)   { { a: 2, b: 3 } }
 
-    it ":method with positional and kws" do
+    it ":method" do
       step = Step.new
 
       # positional = { a: 1 }
@@ -26,22 +39,25 @@ class OptionTest < Minitest::Spec
       # evaluate the option.
       result = option.( positional, keywords, exec_context: step )
 
-      result.must_equal( [{:a=>1}, 2, {:b=>3}] )
-
-      positional.inspect.must_equal %{{:a=>1}}
-      keywords.inspect.must_equal %{{:a=>2, :b=>3}}
+      assert_result result
     end
 
-    it "lambda with positional and kws, WITH flow_options" do
+    it "-> {} lambda" do
       option = Trailblazer::Option(WITH_POSITIONAL_AND_KEYWORDS)
 
       # evaluate the option.
       result = option.( positional, keywords, { exec_context: "something" } )
 
-      result.must_equal( [{:a=>1}, 2, {:b=>3}] )
+      assert_result result
+    end
 
-      positional.inspect.must_equal %{{:a=>1}}
-      keywords.inspect.must_equal %{{:a=>2, :b=>3}}
+    it "callable" do
+      option = Trailblazer::Option(WithPositionalAndKeywords)
+
+      # evaluate the option.
+      result = option.( positional, keywords, { exec_context: "something" } )
+
+      assert_result result
     end
   end
 end
