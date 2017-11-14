1c3c9470b508d7b3551566b2c7a933b1e07d7a01
Nick Sutterer <apotonick@gmail.com>
2017-11-14 14:33:34 +0100

    introduce superclass Builder, which I'm only partly excited about.

--- lib/trailblazer/activity.rb
+++ lib/trailblazer/activity.rb
@@ -200,34 +200,52 @@ module Trailblazer
       end
     end
 
-    class Path
-      def initialize
-        # start, end
-
-      end
-
-      def finalize
-
-      end
-    end
-
     # wir wollen einmal dsl.task von_railway op und einmal DSL.new(andere_sq).instance_exec()
 
 
+    class Builder
+      def initialize(strategy_options={})
+        @strategy_options = strategy_options
+
+        @sequence = Magnetic::Alterations.new
+      end
+
+      def draft
+        @sequence.to_a
+      end
+
+      private
+
+      # merge @strategy_options (for the track colors)
+      # normalize options
+      def add(strategy, task, options, &block)
+        local_options, options = normalize(options, keywords)
+
+        @sequence = DSL.alter_sequence( @sequence, task, options, id: local_options[:id],
+          strategy: [ strategy, @strategy_options.merge( local_options ) ],
+          &block
+        )
+      end
+
+      def normalize(options, local_keys)
+        local, foreign = {}, {}
+        options.each { |k,v| local_keys.include?(k) ? local[k] = v : foreign[k] = v }
+
+        return local, foreign
+      end
+    end
 
 
     module FastTrack
 
     end
-    class FastTrack::Builder
+    class FastTrack::Builder < Builder
       def keywords
         [:id, :plus_poles, :fail_fast, :pass_fast, :fast_track]
       end
 
       def initialize(strategy_options={})
-        @strategy_options = strategy_options
-
-        sequence = Magnetic::Alterations.new
+        sequence = super
         sequence = DSL::PoleGenerator::Path.initialize_sequence(sequence, strategy_options)
         sequence = DSL::PoleGenerator::Railway.initialize_sequence(sequence, strategy_options)
         sequence = DSL::PoleGenerator::FastTrack.initialize_sequence(sequence, strategy_options)
@@ -245,33 +263,11 @@ module Trailblazer
         add(DSL::PoleGenerator::FastTrack.method(:pass), *args, &block)
       end
 
-      def draft
-        @sequence.to_a
-      end
-
       def finalize()
         tripletts = draft
         # pp tripletts
 
         circuit_hash = Trailblazer::Activity::Schema::Magnetic.( tripletts )
-      end
-
-      # merge @strategy_options (for the track colors)
-      # normalize options
-      private def add(strategy, task, options, &block)
-        local_options, options = normalize(options, keywords)
-
-        @sequence = DSL.alter_sequence( @sequence, task, options, id: local_options[:id],
-          strategy: [ strategy, @strategy_options.merge( local_options ) ],
-          &block
-        )
-      end
-
-      private def normalize(options, local_keys)
-        local, foreign = {}, {}
-        options.each { |k,v| local_keys.include?(k) ? local[k] = v : foreign[k] = v }
-
-        return local, foreign
       end
 
       def to_activity
@@ -288,15 +284,13 @@ module Trailblazer
     end
 
     class Path
-      class Builder < FastTrack::Builder
+      class Builder < Builder
         def keywords
           [:id, :plus_poles]
         end
 
         def initialize(strategy_options={})
-          @strategy_options = strategy_options
-
-          sequence = Magnetic::Alterations.new
+          sequence = super
           sequence = DSL::PoleGenerator::Path.initialize_sequence(sequence, strategy_options)
 
           @sequence = sequence
