ea0da98bdd22d658a3114c8ee994369621dff471
Nick Sutterer <apotonick@gmail.com>
2017-12-19 12:52:24 +0100

    silent call_task.

--- lib/trailblazer/wrap/call_task.rb
+++ lib/trailblazer/wrap/call_task.rb
@@ -7,13 +7,13 @@ class Trailblazer::Activity
       task  = wrap_ctx[:task]
 
       # Call the actual task we're wrapping here.
-      puts "~~~~wrap.call: #{task}"
+      # puts "~~~~wrap.call: #{task}"
       result_direction, result_args = task.( *original_args ) # we lose :exec_context here.
 
       # DISCUSS: do we want original_args here to be passed on, or the "effective" result_args which are different to original_args now?
       wrap_ctx = wrap_ctx.merge( result_direction: result_direction, result_args: result_args )
 
-      [ Trailblazer::Activity::Right, [ wrap_ctx, original_args ], **circuit_options ]
+      [ Right, [ wrap_ctx, original_args ], **circuit_options ]
     end
   end # Wrap
 end
