63c5b018fb133ee4baf6c4d4f3a687ae3c84388f
Nick Sutterer <apotonick@gmail.com>
2018-01-18 07:31:40 +0100

    test Activity[ Railway ].

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -197,8 +197,27 @@ ActivityTest::L
         extend Activity[Activity::Railway]
         step A
         step B
+        fail C
       end
+
+      Cct(activity.decompose.first).must_equal %{
+#<Start:default/nil>
+ {Trailblazer::Activity::Right} => ActivityTest::A
+ActivityTest::A
+ {Trailblazer::Activity::Right} => ActivityTest::B
+ {Trailblazer::Activity::Left} => ActivityTest::C
+ActivityTest::B
+ {Trailblazer::Activity::Left} => ActivityTest::C
+ {Trailblazer::Activity::Right} => #<End:success/:success>
+ActivityTest::C
+ {Trailblazer::Activity::Right} => #<End:failure/:failure>
+ {Trailblazer::Activity::Left} => #<End:failure/:failure>
+#<End:success/:success>
+
+#<End:failure/:failure>
+}
     end
+
 
     # normalizer
   end
