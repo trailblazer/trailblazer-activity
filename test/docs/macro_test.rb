16bc14d6800a93b75d28aa7614eb3768e0204934
Nick Sutterer <apotonick@gmail.com>
2018-01-26 07:56:53 +0100

    document macros. specifically, start working on an If macro.

---
+++ test/docs/macro_test.rb
@@ -0,0 +1,51 @@
+require "test_helper"
+
+class DocsMacroTest < Minitest::Spec
+  describe "If()" do
+    let(:activity) do
+      Module.new do
+        extend Activity::Path()
+
+        module_function
+        def find_model(ctx, id:, **)
+          ctx[:model] = Memo.find_by(id: id)
+        end
+        def create(ctx, id:, **)
+          ctx[:model] = Memo.find_by(id: id)
+        end
+        def update(ctx, id:, **)
+          ctx[:model] = Memo.find_by(id: id)
+        end
+        def save(ctx, id:, **)
+          ctx[:model] = Memo.find_by(id: id)
+        end
+
+        task method(:find_model), Output(:failure) => "create", Output(:success) => "update"
+        task method(:create), magnetic_to: [], id: "create"
+        task method(:update), magnetic_to: [], id: "update"
+        task method(:save)
+      end
+    end
+
+    let(:branching) do
+      %{
+#<Start/:default>
+ {Trailblazer::Activity::Right} => #<TaskBuilder{#<Method: #<Trailblazer::Activity: {}>.find_model>}>
+#<TaskBuilder{#<Method: #<Trailblazer::Activity: {}>.find_model>}>
+ {Trailblazer::Activity::Left} => #<TaskBuilder{#<Method: #<Trailblazer::Activity: {}>.create>}>
+ {Trailblazer::Activity::Right} => #<TaskBuilder{#<Method: #<Trailblazer::Activity: {}>.update>}>
+#<TaskBuilder{#<Method: #<Trailblazer::Activity: {}>.create>}>
+ {Trailblazer::Activity::Right} => #<TaskBuilder{#<Method: #<Trailblazer::Activity: {}>.save>}>
+#<TaskBuilder{#<Method: #<Trailblazer::Activity: {}>.update>}>
+ {Trailblazer::Activity::Right} => #<TaskBuilder{#<Method: #<Trailblazer::Activity: {}>.save>}>
+#<TaskBuilder{#<Method: #<Trailblazer::Activity: {}>.save>}>
+ {Trailblazer::Activity::Right} => #<End/:success>
+#<End/:success>
+}
+    end
+
+    it "creates correct activity" do
+      Cct(activity.to_h[:circuit], inspect_task: Activity::Introspect.method(:inspect_task_builder) ).must_equal branching
+    end
+  end
+end
