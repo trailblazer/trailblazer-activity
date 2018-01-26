3cb746bd62696179c8f147f5b298d85cc89f46b9
Nick Sutterer <apotonick@gmail.com>
2018-01-26 18:44:10 +0100

    re-add torture snippets. no idea where these got lost.

--- test/docs/activity_test.rb
+++ test/docs/activity_test.rb
@@ -13,41 +13,58 @@ class DocsActivityTest < Minitest::Spec
 
   describe "#what" do
     it do
-        module Update
-          extend Trailblazer::Activity::Railway()
-          module_function
+      #:overview
+      module Memo::Update
+        extend Trailblazer::Activity::Railway()
 
-          # here goes your business logic
-          #
-          def find_model(ctx, id:, **)
-            ctx[:model] = Memo.find_by(id: id)
-          end
-
-          def validate(ctx, params:, **)
-            return true if params[:body].is_a?(String) && params[:body].size > 10
-            ctx[:errors] = "body not long enough"
-            false
-          end
-
-          def save(ctx, model:, params:, **)
-            model.update_attributes(params)
-          end
-
-          def log_error(ctx, params:, **)
-            ctx[:log] = "Some idiot wrote #{params.inspect}"
-          end
-
-          # here comes the DSL describing the layout of the activity
-          #
-          step method(:find_model)
-          step method(:validate), Output(:failure) => End(:validation_error)
-          step method(:save)
-          fail method(:log_error)
+        module_function
+        #
+        # here goes your business logic
+        #
+        def find_model(ctx, id:, **)
+          ctx[:model] = Memo.find_by(id: id)
         end
 
+        def validate(ctx, params:, **)
+          return true if params[:body].is_a?(String) && params[:body].size > 10
+          ctx[:errors] = "body not long enough"
+          false
+        end
+
+        def save(ctx, model:, params:, **)
+          model.update_attributes(params)
+        end
+
+        def log_error(ctx, params:, **)
+          ctx[:log] = "Some idiot wrote #{params.inspect}"
+        end
+        #
+        # here comes the DSL describing the layout of the activity
+        #
+        step method(:find_model)
+        step method(:validate), Output(:failure) => End(:validation_error)
+        step method(:save)
+        fail method(:log_error)
+      end
+      #:overview end
+
+      #:overview-call
       ctx = { id: 1, params: { body: "Awesome!" } }
 
-      event, (ctx, *) = Update.( [ctx, {}] )
+      event, (ctx, *) = Memo::Update.( [ctx, {}] )
+      #:overview-call end
+=begin
+      #:overview-result
+      pp ctx #=>
+      {:id=>1,
+       :params=>{:body=>"Awesome!"},
+       :model=>#<struct DocsActivityTest::Memo body=nil>,
+       :errors=>"body not long enough"}
+
+      puts signal #=> #<Trailblazer::Activity::End semantic=:validation_error>
+      #:overview-result end
+=end
+      ctx.inspect.must_equal %{{:id=>1, :params=>{:body=>\"Awesome!\"}, :model=>#<struct DocsActivityTest::Memo body=nil>, :errors=>\"body not long enough\"}}
 
       pp ctx
       pp event
