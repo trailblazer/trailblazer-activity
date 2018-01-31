c468e552a83ac8073f418f32a26315f4c80afe53
Nick Sutterer <apotonick@gmail.com>
2018-01-31 17:19:33 +0100

    add failing tests.

--- test/docs/output_test.rb
+++ test/docs/output_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
 
 class DocsOutputTest < Minitest::Spec
+  let(:nested) do
+    Module.new do
+      extend Activity::FastTrack()
+
+      step T.def_task(:a), fast_track: true # four ends.
+    end
+  end
+
   describe "Output() with task: Activity" do
     it "allows to grab existing Output(:semantic) from nested activity" do
-      nested = Module.new do
-        extend Activity::FastTrack()
-
-        step T.def_task(:a), fast_track: true # four ends.
-      end
+      nested = self.nested
 
       activity = Module.new do
         extend Activity::Path()
@@ -15,6 +19,43 @@ class DocsOutputTest < Minitest::Spec
         task Activity::DSL::Helper::Nested(nested),
           Output(:pass_fast) => End(:my_pass_fast) # references a plus pole from VV
       end
+
+      Cct(activity.to_h[:circuit]).must_equal %{
+#<Start/:default>
+ {Trailblazer::Activity::Right} => #<Trailblazer::Activity: {}>
+#<Trailblazer::Activity: {}>
+ {#<Trailblazer::Activity::End semantic=:success>} => #<End/:success>
+ {#<Trailblazer::Activity::End semantic=:pass_fast>} => #<End/:my_pass_fast>
+
+
+#<End/:success>
+
+#<End/:my_pass_fast>
+}
+    end
+
+    it "allows adding an additional plus pole to Nested's outputs" do
+      nested = self.nested
+
+      activity = Module.new do
+        extend Activity::Path()
+
+        task Activity::DSL::Helper::Nested(nested),
+          Output("Restart", :restart) => End(:restart) # references a plus pole from VV
+      end
+
+      Cct(activity.to_h[:circuit]).must_equal %{
+#<Start/:default>
+ {Trailblazer::Activity::Right} => #<Trailblazer::Activity: {}>
+#<Trailblazer::Activity: {}>
+ {#<Trailblazer::Activity::End semantic=:success>} => #<End/:success>
+ {Restart} => #<End/:restart>
+
+
+#<End/:success>
+
+#<End/:restart>
+}
     end
   end
 end
