cfaee8cab426faea7c1a66b40c888a4674a2cd51
Nick Sutterer <apotonick@gmail.com>
2018-03-23 15:18:40 +0100

    breaking test for path with pass and Left.

--- test/docs/path_test.rb
+++ test/docs/path_test.rb
@@ -36,4 +36,31 @@ class DocsPathTest < Minitest::Spec
       ctx.must_equal({:seq=>[:find, :create, :populate, :save], :find_return=>false})
     end
   end
+
+  describe "#pass" do
+    it do
+      module Memo::Create
+        extend Trailblazer::Activity::Path()
+        #~methods
+        extend T.def_steps(:save)
+        def self.find(ctx, find_return:, seq:, **)
+          seq << :find
+          find_return
+        end
+        #~methods end
+
+        pass method(:find)
+        pass method(:save)
+      end
+
+      pp Memo::Create.to_h[:circuit]
+
+      signal, (ctx, _) = Memo::Create.( [{ seq: [], find_return: true }] )
+      ctx.must_equal({:seq=>[:find, :save], :find_return=>true})
+
+      # both return values go straight to the next task
+      signal, (ctx, _) = Memo::Create.( [{ seq: [], find_return: false }] )
+      ctx.must_equal({:seq=>[:find, :save], :find_return=>true})
+    end
+  end
 end
