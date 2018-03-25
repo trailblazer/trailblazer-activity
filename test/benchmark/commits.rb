a936cbf9a49bf5b8c1d9fec09382ec7f05011d84
Nick Sutterer <apotonick@gmail.com>
2018-03-25 11:31:46 +0200

    cool, add a benchmarker for different git versions.

--- test/benchmark/commits.rb
+++ test/benchmark/commits.rb
@@ -1,22 +1,60 @@
-require "test_helper"
-
-gem "benchmark-ips"
 require "benchmark/ips"
+require "trailblazer/activity"
 
 =begin
 ## Learning
 
 =end
 
+
+module Form
+  extend Trailblazer::Activity::Path()
+
+  module_function
+
+  def parse(ctx, model:, **)
+    model[:email] = "yo@trb.to"
+  end
+
+  task method(:parse)
+end
+
+module Create
+  extend Trailblazer::Activity::Path()
+
+  module_function
+
+  def model(ctx, **)
+    ctx[:model] = {}
+  end
+
+  def save(ctx, model:, **)
+    model[:save] = true
+  end
+
+  task method(:model)
+  task Subprocess(Form)
+  task method(:save)
+end
+
+def run
+  signal, (ctx, _) = Create.({})
+  # pp ctx
+end
+
 commits = {
+  "0.5.2" => "v0.5.2",
   "0.5.3" => "v0.5.3",
 }
 
 
 Benchmark.ips do |x|
 
-  x.report("old_circuit_api_doublesplat") { old_circuit_api_doublesplat(old_signature, circuit_options) }
-  x.report("old_circuit_api")             { old_circuit_api(old_signature, circuit_options) }
+  commits.values[-2..-1].each do |tag|
+    puts `git checkout #{tag}`
+
+    x.report(tag) { run }
+  end
 
   x.compare!
 end
