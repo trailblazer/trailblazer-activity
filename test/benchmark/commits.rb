e80dc640b07b995dad7dea8bf3c328a9988d2693
Nick Sutterer <apotonick@gmail.com>
2018-03-25 11:55:40 +0200

    make benchmarker really checkout different versions.

--- test/benchmark/commits.rb
+++ test/benchmark/commits.rb
@@ -1,14 +1,20 @@
-
+# be ruby test/benchmark/commits.rb
 
 commits = {
-  "0.5.3" => "v0.5.3",
-  "0.5.3" => "master",
+  "ref-1" => "master",
+  "ref-2" => "master",
 }
 
-commits.values[-1..-1].each do |tag|
-  puts `git checkout #{tag}`
-  result = `bundle exec ruby test/benchmark/ips.rb`
-  raise result.inspect
-end
+
+results =
+  commits.values[-2..-1].collect do |tag|
+    puts `git checkout #{tag}`
+    result = `bundle exec ruby test/benchmark/ips.rb`
+    result = result.split("\n").last.split("-").last
+
+    [tag, result]
+  end
+
+pp results
 
 `git checkout master`
