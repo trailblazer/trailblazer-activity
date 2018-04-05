95100e34ce9a89b47c12e93cec9482dcfe31e062
Nick Sutterer <apotonick@gmail.com>
2018-04-05 10:06:53 +0200

    move #def_steps to Testing.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -55,33 +55,5 @@ Trailblazer::Activity.module_eval do
   end
 end
 
-# Helpers to quickly create steps and tasks.
-module T
-  # Creates a module with one step method for each name.
-  #
-  # @example
-  #   extend T.def_steps(:create, :save)
-  def self.def_steps(*names)
-    Module.new do
-      names.each do |name|
-        define_method(name) do | ctx, ** |
-          ctx[:seq] << name
-          ctx.key?(name) ? ctx[name] : true
-        end
-      end
-    end
-  end
-
-  # Creates a method instance with a task interface.
-  #
-  # @example
-  #   task task: T.def_task(:create)
-  def self.def_task(name)
-    Module.new do
-      define_singleton_method(name) do | (ctx, flow_options), ** |
-        ctx[:seq] << name
-        return Trailblazer::Activity::Right, [ctx, flow_options]
-      end
-    end.method(name)
-  end
-end
+require "trailblazer/activity/testing"
+T = Trailblazer::Activity::Testing
