fcaaeb3538d1445297c93f042d965cc7446defcd
Nick Sutterer <apotonick@gmail.com>
2019-02-21 20:04:47 +0100

    fuck yeah, we can compile an "Acti" including taskWrap extensions!!!!

--- test/wrap/task_wrap_test.rb
+++ test/wrap/task_wrap_test.rb
@@ -2,21 +2,20 @@ require "test_helper"
 
 # Test taskWrap concepts along with {Instance}s.
 class TaskWrapTest < Minitest::Spec
-  TaskWrap = Trailblazer::Activity::TaskWrap
+  TaskWrap  = Trailblazer::Activity::TaskWrap
+  Config    = Trailblazer::Activity::State::Config
 
   it "what" do
     intermediate = Inter.new(
       {
         Inter::TaskRef(:a) => [Inter::Out(:success, :b)],
         Inter::TaskRef(:b) => [Inter::Out(:success, :c)],
-        Inter::TaskRef(:c) => [Inter::Out(:success, "End.failure")],
+        Inter::TaskRef(:c) => [Inter::Out(:success, "End.success")],
         Inter::TaskRef("End.success", stop_event: true) => [Inter::Out(:success, nil)]
       },
-      [Inter::TaskRef("End.success"), Inter::TaskRef("End.failure")],
+      [Inter::TaskRef("End.success")],
       [Inter::TaskRef(:a)] # start
     )
-
-    Config = Trailblazer::Activity::State::Config
 
     merge = [
       [TaskWrap::Pipeline.method(:insert_before), "task_wrap.call_task", ["user.add_1", method(:add_1)]],
@@ -35,14 +34,15 @@ class TaskWrapTest < Minitest::Spec
 
     implementation = {
       :a => Schema::Implementation::Task(a = implementing.method(:a), [Activity::Output(Activity::Right, :success)],                 [TaskWrap::Extension.new(task: a, merge: InitializeStaticWrap),
-                                                                                                                                  TaskWrap::Extension(merge: merge, task: a)]),
-      :b => Schema::Implementation::Task(b = implementing.method(:b), [Activity::Output("B/success", :success)],                     [TaskWrap::Extension.new(task: b, merge: InitializeStaticWrap)]),
+                                                                                                                                      TaskWrap::Extension(merge: merge, task: a)]),
+      :b => Schema::Implementation::Task(b = implementing.method(:b), [Activity::Output(Activity::Right, :success)],                 [TaskWrap::Extension.new(task: b, merge: InitializeStaticWrap)]),
       :c => Schema::Implementation::Task(c = implementing.method(:c), [Activity::Output(Activity::Right, :success)],                 [TaskWrap::Extension.new(task: c, merge: InitializeStaticWrap)]),
-      "End.success" => Schema::Implementation::Task(implementing::Success, [Activity::Output(implementing::Success, :success)]), # DISCUSS: End has one Output, signal is itself?
-      "End.failure" => Schema::Implementation::Task(implementing::Failure, [Activity::Output(implementing::Failure, :failure)]),
+      "End.success" => Schema::Implementation::Task(es = implementing::Success, [Activity::Output(implementing::Success, :success)], [TaskWrap::Extension.new(task: es, merge: InitializeStaticWrap)]), # DISCUSS: End has one Output, signal is itself?
     }
 
     schema = Inter.(intermediate, implementation)
+
+
 
     class Acti
       def initialize(schema)
@@ -54,11 +54,12 @@ class TaskWrapTest < Minitest::Spec
       end
 
       def [](*key)
-        puts "@@@@@ #{key.inspect}"
         @schema[:config][*key]
       end
     end
 
-    pp TaskWrap.invoke(Acti.new(schema), [{seq: []}], **{})
+    signal, (ctx, flow_options) = TaskWrap.invoke(Acti.new(schema), [{seq: []}], **{})
+
+    ctx.inspect.must_equal %{{:seq=>[1, :a, 2, :b, :c]}}
   end
 end
