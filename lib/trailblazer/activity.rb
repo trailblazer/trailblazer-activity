a8d8c2e42215ab3b990f5fccd9cb5294d94ac980
Nick Sutterer <apotonick@gmail.com>
2019-02-22 08:47:33 +0100

    {Activity} is now an instance and represents an invokable process with {config} that provides {wrap_static}.

--- lib/trailblazer/activity.rb
+++ lib/trailblazer/activity.rb
@@ -1,28 +1,29 @@
 module Trailblazer
-  class Activity < Module
-    attr_reader :initial_state
+  class Activity
+    def initialize(schema)
+      @schema = schema
+    end
 
-    def initialize(implementation, options)
-      builder, adds, process, outputs_map, options = BuildState.build_state_for( implementation.config, options)
-
-      @initial_state = State::Config.build(
-        builder: builder,
-        options: options,
-        adds:    adds,
-        process: process,
-        circuit: process.circuit,
-        outputs: outputs_map,
+    def call(args, circuit_options={})
+      @schema[:circuit].(
+        args,
+        circuit_options.merge(activity: self)
       )
-
-      include *options[:extend] # include the DSL methods.
-      include PublicAPI
     end
 
-    # Injects the initial configuration into the module defining a new activity.
-    def extended(extended)
-      super
-      extended.instance_variable_set(:@state, initial_state)
+    # Reader and writer method for an Activity.
+    # The writer {dsl[:key] = "value"} exposes immutable behavior and will replace the old
+    # @state with a new, modified copy.
+    #
+    # Always use the accessors to avoid leaking state to other components
+    # due to mutable write operations.
+    def [](*key)
+      @schema[:config][*key]
     end
+
+
+
+
 
 
     module Inspect
@@ -34,45 +35,16 @@ module Trailblazer
     end
 
 
-    # Helpers such as Path, Output, End to be included into {Activity}.
-    # module DSLHelper
-    #   extend Forwardable
-    #   def_delegators DSL, :Output, :End, :Subprocess, :Track
 
-    #   def Path(*args, &block)
-    #     self[:builder].Path(*args, &block)
-    #   end
-    # end
 
-    # Reader and writer method for DSL objects.
-    # The writer {dsl[:key] = "value"} exposes immutable behavior and will replace the old
-    # @state with a new, modified copy.
-    #
-    # Always use the DSL::Accessor accessors to avoid leaking state to other components
-    # due to mutable write operations.
-    module Accessor
-      def []=(*args)
-        @state = State::Config.send(:[]=, @state, *args)
-      end
-
-      def [](*args)
-        State::Config[@state, *args]
-      end
-    end
 
     # FIXME: still to be decided
     # By including those modules, we create instance methods.
     # Later, this module is `extended` in Path, Railway and FastTrack, and
     # imports the DSL methods as class methods.
     module PublicAPI
-      include Accessor
-
-
-
       require "trailblazer/activity/interface"
       include Activity::Interface # DISCUSS
-
-      # include DSLHelper # DISCUSS
 
       include Activity::Inspect # DISCUSS
 
@@ -80,9 +52,7 @@ module Trailblazer
       # include Magnetic::Merge # Activity#merge!
 
       # @private Note that {Activity.call} is considered private until the public API is stable.
-      def call(args, circuit_options={})
-        self[:circuit].( args, circuit_options.merge(activity: self) )
-      end
+
     end
   end # Activity
 end
