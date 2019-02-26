84837ebd51e62ee2721868b91f904520da4c336d
Nick Sutterer <apotonick@gmail.com>
2019-02-26 13:18:38 +0100

    fix all introspection_test tests.

--- test/introspection_test.rb
+++ test/introspection_test.rb
@@ -1,29 +1,30 @@
 require "test_helper"
 
 class IntrospectionTest < Minitest::Spec
+
   describe "Introspect::Graph" do
     let(:graph) { graph = Activity::Introspect::Graph(nested_activity) }
 
     describe "#find" do
-      let(:node) { graph.find("B") }
-      it { node[:id].must_equal "B" }
-      it { assert_outputs(node, success: Right, failure: Left) }
-      it { node[:task].must_equal B }
-      it { node[:outgoings].inspect.must_equal(%{[#<struct Trailblazer::Activity::Introspect::Graph::Outgoing output=#<struct Trailblazer::Activity::Output signal=Trailblazer::Activity::Right, semantic=:success>, task=#<Trailblazer::Activity::End semantic=:success>>, #<struct Trailblazer::Activity::Introspect::Graph::Outgoing output=#<struct Trailblazer::Activity::Output signal=Trailblazer::Activity::Left, semantic=:failure>, task=#<Trailblazer::Activity::End semantic=:success>>]}) }
+      let(:node) { graph.find(:B) }
+      it { node[:id].must_equal :B }
+      it { assert_outputs(node, success: Activity::Right) }
+      it { node[:task].must_equal implementing.method(:b) }
+      it { node[:outgoings].inspect.must_equal(%{[#<struct Trailblazer::Activity::Introspect::Graph::Outgoing output=#<struct Trailblazer::Activity::Output signal=Trailblazer::Activity::Right, semantic=:success>, task=#{bc}>]}) }
 
       describe "with Start.default" do
         let(:node) { graph.find("Start.default") }
         it { node[:id].must_equal "Start.default" }
-        it { assert_outputs(node, success: Right) }
-        it { node[:task].must_equal activity.to_h[:circuit].to_h[:start_task] }
+        it { assert_outputs(node, success: Activity::Right) }
+        it { node[:task].must_equal nested_activity.to_h[:circuit].to_h[:start_task] }
       end
 
       describe "with block" do
-        let(:node) { graph.find { |node| node[:task] == B } }
+        let(:node) { graph.find { |node| node[:task] == implementing.method(:b) } }
 
-        it { node[:id].must_equal "B" }
-        it { node[:task].must_equal B }
-        it { assert_outputs(node, success: Right, failure: Left) }
+        it { node[:id].must_equal :B }
+        it { node[:task].must_equal implementing.method(:b) }
+        it { assert_outputs(node, success: Activity::Right) }
       end
     end
 
@@ -31,16 +32,18 @@ class IntrospectionTest < Minitest::Spec
       it do
         nodes = graph.collect { |node| node }
 
-        nodes.size.must_equal 4
+        nodes.size.must_equal 5
 
         nodes[0][:task].inspect.must_equal %{#<Trailblazer::Activity::Start semantic=:default>}
-        assert_outgoings nodes[0], Activity::Right=>"I am not callable!"
-        nodes[1][:task].must_equal "I am not callable!"
-        assert_outgoings nodes[1], Activity::Right=>B, Activity::Left=>B
-        nodes[2][:task].must_equal B
-        assert_outgoings nodes[2], Activity::Right=>nodes[3].task, Activity::Left=>nodes[3].task
-        nodes[3][:task].inspect.must_equal %{#<Trailblazer::Activity::End semantic=:success>}
-        assert_outgoings nodes[3], {}
+        assert_outgoings nodes[0], Activity::Right=>implementing.method(:b)
+        nodes[1][:task].must_equal implementing.method(:b)
+        assert_outgoings nodes[1], Activity::Right=>bc
+        nodes[2][:task].must_equal bc
+        assert_outgoings nodes[2], bc.to_h[:outputs][0].signal => nodes[3].task
+        nodes[3][:task].must_equal implementing.method(:f)
+        assert_outgoings nodes[3], Activity::Right => nested_activity.to_h[:outputs][0].signal
+        nodes[4][:task].inspect.must_equal %{#<Trailblazer::Activity::End semantic=:success>}
+        assert_outgoings nodes[4], {}
       end
     end
 
