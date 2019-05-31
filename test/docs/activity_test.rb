cb029e60dd22ab832057b5e0ecabc1b80bac9d18
Nick Sutterer <apotonick@gmail.com>
2019-05-31 21:18:06 +0200

    add doc/imp test.

--- test/docs/activity_test.rb
+++ test/docs/activity_test.rb
@@ -3,14 +3,14 @@ require "test_helper"
 class DocsActivityTest < Minitest::Spec
   it do
     #:int
-    Intermediate = Trailblazer::Activity::Intermediate # shortcut alias.
+    Intermediate = Trailblazer::Activity::Schema::Intermediate # shortcut alias.
 
     intermediate = Intermediate.new(
       {
         Intermediate::TaskRef(:"Start")  => [Intermediate::Out(:success, :A)],
         Intermediate::TaskRef(:A)        => [Intermediate::Out(:success, :B),
                                              Intermediate::Out(:failure, :C)],
-        Intermediate::TaskRef(:B)        => [Intermediate::Out(:success, "End.success")],
+        Intermediate::TaskRef(:B)        => [Intermediate::Out(:success, :"End")],
         Intermediate::TaskRef(:C)        => [Intermediate::Out(:success, :B)],
         Intermediate::TaskRef(:"End", stop_event: true) => [Intermediate::Out(:success, nil)]
       },
@@ -19,14 +19,41 @@ class DocsActivityTest < Minitest::Spec
     )
     #:int end
 
+
+    #:impl-mod
+    module Upsert
+      module_function
+
+      def a((ctx, flow_options), *)
+        ctx[:seq] << :a
+        return Trailblazer::Activity::Right, [ctx, flow_options]
+      end
+
+      #~mod
+      extend T.def_tasks(:b, :c)
+      #~mod end
+    end
+
+    start = Activity::Start.new(semantic: :default)
+    _end  = Activity::End.new(semantic: :success)
+    #:impl-mod end
+
+    #:impl
+    Activity = Trailblazer::Activity # shortcut alias.
+    Implementation = Trailblazer::Activity::Schema::Implementation
+
     implementation = {
-      "Start.default" => Schema::Implementation::Task(st = implementing::Start, [Activity::Output(Activity::Right, :success)],        []),
-      :C => Schema::Implementation::Task(c = C, [Activity::Output(Activity::Right, :success)],                                        []),
-      "End.success" => Schema::Implementation::Task(_es = implementing::Success, [Activity::Output(implementing::Success, :success)], []), # DISCUSS: End has one Output, signal is itself?
+      :"Start"  => Implementation::Task(start,             [Activity::Output(Activity::Right, :success)], []),
+      :A        => Implementation::Task(Upsert.method(:c), [Activity::Output(Activity::Right, :success),
+                                                            Activity::Output(Activity::Left, :failure)],  []),
+      :B        => Implementation::Task(Upsert.method(:c), [Activity::Output(Activity::Right, :success)], []),
+      :C        => Implementation::Task(Upsert.method(:c), [Activity::Output(Activity::Right, :success)], []),
+      :"End"    => Implementation::Task(_end, [Activity::Output(_end, :success)],                         []),
     }
+    #:impl end
 
     schema = Inter.(intermediate, implementation)
 
-    Activity.new(schema)
+    activity = Activity.new(schema)
   end
 end
