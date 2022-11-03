8d444b91a45558f115414e82ac16650606c6c2f5
Nick Sutterer <apotonick@gmail.com>
2022-11-03 19:35:49 +0100

    Start working on Circuit::Task classes so we finally have a clean, understandable abstraction for all things "TaskBuilder".

--- lib/trailblazer/activity/circuit/task_adapter.rb
+++ lib/trailblazer/activity/circuit/task_adapter.rb
@@ -1,14 +1,80 @@
 module Trailblazer
   class Activity
     class Circuit
-      module TaskAdapter
+      def self.Step(callable_with_step_interface, option: false)
+        if option
+          return Step::Option.new(Trailblazer::Option(callable_with_step_interface), callable_with_step_interface)
+        end
+
+        Step.new(callable_with_step_interface, callable_with_step_interface) # DISCUSS: maybe we can ditch this, only if performance is cool here, but where would we use that?
+      end
+
+      # {Step#call} returns a return value. It is *not* circuit-interface compatible (by design).
+      class Step # DISCUSS: Will WE NEED/USE this anywhere? we normally wrap everything into an Option, anyway.
+        def initialize(step, user_proc, **)
+          @step            = step
+          @user_proc       = user_proc
+        end
+
+        # Execute the user step with TRB's kw args.
+        # {@step} is/implements {Trailblazer::Option} interface.
+        def call((ctx, flow_options), **circuit_options)
+          result = @step.call(ctx, **ctx.to_hash)
+          # in an immutable environment we should return the ctx from the step.
+          return result, ctx
+        end
+
+        # In {Step::Option}, {@step} is expected to be wrapped in an {Option}.
+        # To remember: when calling an Option instance, you need to pass {:keyword_arguments} explicitely, because, Ruby 2.5 and 2.6.
+        #
+        # This is often needed for "decider" chunks where the user can run either a method or a callable
+        # but you only want back the return value, not a Binary circuit-interface return set.
+        class Option < Step
+          def call((ctx, _flow_options), **circuit_options)
+            result = @step.(ctx, keyword_arguments: ctx.to_hash, **circuit_options) # circuit_options contains :exec_context.
+            # in an immutable environment we should return the ctx from the step.
+            return result, ctx
+          end
+        end
+      end
+
+
+        # # Invoke the original {user_proc} that is wrapped in an {Option()}.
+        # private def call_option(step_with_option_interface, (ctx, _flow_options), **circuit_options)
+        #   step_with_option_interface.(ctx, keyword_arguments: ctx.to_hash, **circuit_options) # circuit_options contains :exec_context.
+        # end
+
+      # Always exposes circuit-interface.
+        # that can be used directly in a {Circuit}.
+      class Task
+        def self.for_step(step, binary: true, **options_for_step)
+          circuit_step = Circuit.Step(step, **options_for_step)
+
+          new(circuit_step)
+        end
+
+        # @param circuit_step Exposes a Circuit::Step.call([ctx, flow_options], **circuit_options) interface
+        def initialize(circuit_step, **)
+          @circuit_step = circuit_step
+          # @user_proc       = user_proc
+        end
+
+        def call((ctx, flow_options), **circuit_options)
+          result, ctx = @circuit_step.([ctx, flow_options], **circuit_options)
+
+          # Return an appropriate signal which direction to go next.
+          signal = Task.binary_signal_for(result, Activity::Right, Activity::Left)
+
+          return signal, [ctx, flow_options]
+        end
 
         # every step is wrapped by this proc/decider. this is executed in the circuit as the actual task.
         # Step calls step.(options, **options, flow_options)
         # Output signal binary: true=>Right, false=>Left.
         # Passes through all subclasses of Direction.~~~~~~~~~~~~~~~~~
+
         def self.Binary(user_proc, adapter_class: Step, **options)
-          adapter_class.new(Trailblazer::Option(user_proc), user_proc, **options).freeze
+raise "implement me for compat"
         end
 
         # Translates the return value of the user step into a valid signal.
@@ -21,59 +87,35 @@ module Trailblazer
           end
         end
 
-        # Wraps a {task} (that usually expects the task interface) into a circuit interface
-        # that can be used directly in a {Circuit}.
-        # We expect {task} to be exposing an {Option()} interface when calling it.
-        class Step
-          def initialize(task, user_proc, **)
-            @task            = task
-            @user_proc       = user_proc
-          end
-
-          def call((ctx, flow_options), **circuit_options)
-            # Execute the user step with TRB's kw args.
-            # {@task} is/implements {Trailblazer::Option} interface.
-            result = call_option(@task, [ctx, flow_options], **circuit_options)
-
-            # Return an appropriate signal which direction to go next.
-            signal = Activity::TaskBuilder.binary_signal_for(result, Activity::Right, Activity::Left)
-
-            return signal, [ctx, flow_options]
-          end
-
-          # Invoke the original {user_proc} that is wrapped in an {Option()}.
-          private def call_option(task_with_option_interface, (ctx, _flow_options), **circuit_options)
-            task_with_option_interface.(ctx, keyword_arguments: ctx.to_hash, **circuit_options) # circuit_options contains :exec_context.
-          end
-
-          def inspect # TODO: make me private!
-            %{#<Trailblazer::Activity::TaskBuilder::Task user_proc=#{Trailblazer::Activity::Introspect.render_task(@user_proc)}>}
-          end
-          alias_method :to_s, :inspect
+        def inspect # TODO: make me private!
+          %{#<Trailblazer::Activity::TaskBuilder::Task user_proc=#{Trailblazer::Activity::Introspect.render_task(@user_proc)}>}
+        end
+        alias_method :to_s, :inspect
 
 
-          # Task::Circuit::Adapter::AssignVariable
-          # Run {user_proc} with "step interface" and assign its return value to ctx[@variable_name].
-          class AssignVariable < Step
-            def initialize(*args, variable_name:, **options)
-              super(*args, **options)
+        # Task::Circuit::Adapter::AssignVariable
+        # Run {user_proc} with "step interface" and assign its return value to ctx[@variable_name].
+        # @private
+        # This is experimental.
+        # class AssignVariable < Step
+        #   def initialize(*args, variable_name:, **options)
+        #     super(*args, **options)
 
-              # name of the ctx variable we want to assign the return_value of {user_proc} to.
-              @variable_name = variable_name
-            end
+        #     # name of the ctx variable we want to assign the return_value of {user_proc} to.
+        #     @variable_name = variable_name
+        #   end
 
-            def call_option(task_with_option_interface, (ctx, flow_options), **circuit_options)
-              return_value = super # Adapter.call
+        #   def call_option(task_with_option_interface, (ctx, flow_options), **circuit_options)
+        #     return_value = super # Adapter.call
 
-              ctx[@variable_name] = return_value
-            end
-          end
-        end # Adapter
-      end # Task
+        #     ctx[@variable_name] = return_value
+        #   end
+        # end
+      end
     end # Circuit
 
     # TODO: deprecate
-    TaskBuilder       = Circuit::TaskAdapter
-    TaskBuilder::Task = Circuit::TaskAdapter::Step
+    # TaskBuilder       = Circuit::TaskAdapter
+    # TaskBuilder::Task = Circuit::TaskAdapter::Step
   end # Activity
 end
--- test/task_adapter_test.rb
+++ test/task_adapter_test.rb
@@ -1,6 +1,98 @@
 require "test_helper"
 
 class TaskAdapterTest < Minitest::Spec
+  it "Circuit::TaskAdapter" do
+    class Operation
+      def process_type(ctx, model:, **)
+        return if model.nil?
+
+        ctx[:type] = model.class
+      end
+    end
+
+
+    # steps receive ctx and kwargs
+    # steps can write to something
+    # steps return an outcome "boolean"/return value
+    step = ->(ctx, model:, **) do
+      return if model.nil?
+
+      ctx[:type] = model.class
+    end
+
+    flow_options    = {}
+    circuit_options = {}
+
+    ctx             = {model: nil}
+
+  # execute step in a circuit-interface environment
+    circuit_step = Activity::Circuit::Step(step) # circuit step receives circuit-interface but returns only value
+    return_value, ctx = circuit_step.([ctx, flow_options], **circuit_options)
+
+    assert_nil return_value
+    assert_equal ctx.inspect, %{{:model=>nil}}
+
+
+  #@ execute step-option in a circuit-interface environment
+    circuit_step = Activity::Circuit.Step(step, option: true) # wrap step with Trailblazer::Option
+    return_value, ctx = circuit_step.([ctx, flow_options], **circuit_options, exec_context: self)
+    assert_nil return_value
+    assert_equal ctx.inspect, %{{:model=>nil}}
+
+    circuit_step = Activity::Circuit::Step(:process_type, option: true) # wrap step with Trailblazer::Option
+    return_value, ctx = circuit_step.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
+    assert_nil return_value
+    assert_equal ctx.inspect, %{{:model=>nil}}
+
+  #@ circuit-interface Option-compatible Step that does a Binary signal decision
+    step          = :process_type
+    circuit_task  = Activity::Circuit::Task.for_step(step, option: true)
+
+    ctx = {model: nil}
+    signal, (ctx, flow_options) = circuit_task.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
+
+    assert_equal signal, Trailblazer::Activity::Left
+    assert_equal ctx.inspect, %{{:model=>nil}}
+    assert_equal flow_options.inspect, %{{}}
+
+    ctx = {model: Object}
+    signal, (ctx, flow_options) = circuit_task.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
+
+    assert_equal signal, Trailblazer::Activity::Right
+    assert_equal ctx.inspect, %{{:model=>Object, :type=>Class}}
+    assert_equal flow_options.inspect, %{{}}
+
+#@ pipeline-interface
+
+    step = :process_type
+    args = [1,2]
+
+    pipeline_step = Activity::Circuit::Task.for_step(step, option: true) # Task receives circuit-interface but it's compatible with Pipeline interface
+    wrap_ctx, args = pipeline_step.([ctx, args], exec_context: Operation.new) # that's how pipeline tasks are called in {TaskWrap::Pipeline}.
+
+    assert_equal wrap_ctx.inspect, %{{}}
+    assert_equal args, [1,2]
+
+
+
+raise
+
+
+
+
+
+
+
+    Activity::Circuit::TaskAdapter.Binary
+    Activity::Circuit::Task.for_step(circuit_step) # compute the return value (and also return the mutated object)
+
+    # Circuit consists of {Circuit::Task} objects (in a typed environment)
+
+    # execute step in a pipeline environment
+    Activity::Pipeline.TaskAdapter::Value # compute the return value (and also return the mutated object)
+  end
+
+
   describe "#inspect" do
     it { assert_equal Activity::TaskBuilder.Binary(:imaproc).inspect, %{#<Trailblazer::Activity::TaskBuilder::Task user_proc=imaproc>} }
 
