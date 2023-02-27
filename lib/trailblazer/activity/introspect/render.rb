04bef7c65ae37e847b2977bc6bbb32e9e2a6fee3
Nick Sutterer <apotonick@gmail.com>
2023-02-27 17:06:54 +0100

    add Introspect::Render.

---
+++ lib/trailblazer/activity/introspect/render.rb
@@ -0,0 +1,52 @@
+module Trailblazer
+  class Activity
+    module Introspect
+      module Render
+        module_function
+
+        def call(activity, **options)
+          nodes       = Introspect.Nodes(activity)
+          circuit_map = activity.to_h[:circuit].to_h[:map]
+
+          content = nodes.collect do |task, node|
+            outgoings = circuit_map[task]
+
+            conns = outgoings.collect do |signal, target|
+              " {#{signal}} => #{inspect_with_matcher(target, **options)}"
+            end
+
+            [
+              inspect_with_matcher(node.task, **options),
+              conns.join("\n")
+            ]
+          end
+
+          content = content.join("\n")
+
+          "\n#{content}".gsub(/0x\w+/, "0x")
+        end
+
+        # If Ruby had pattern matching, this function wasn't necessary.
+        def inspect_with_matcher(task, inspect_task: method(:inspect_task), inspect_end: method(:inspect_end))
+          return inspect_task.(task) unless task.kind_of?(Trailblazer::Activity::End)
+          inspect_end.(task)
+        end
+
+        def inspect_task(task)
+          task.inspect
+        end
+
+        def inspect_end(task)
+          class_name = strip(task.class)
+          options    = task.to_h
+
+          "#<#{class_name}/#{options[:semantic].inspect}>"
+        end
+
+        def strip(string)
+          string.to_s.sub("Trailblazer::Activity::", "")
+        end
+      end
+    end
+  end
+end
