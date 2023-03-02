f1ab718ec2587bf4a2b44ad2dfdc7ce3a33bfc99
Nick Sutterer <apotonick@gmail.com>
2023-03-02 10:33:01 +0100

    docs tests for Deprecate.

--- test/docs/internals_test.rb
+++ test/docs/internals_test.rb
@@ -1,12 +1,15 @@
 require "test_helper"
 
 class DocInternalsTest < Minitest::Spec
+  #:method
   def outdated_method
     Trailblazer::Activity::Deprecate.warn caller_locations[0], "The `#outdated_method` is deprecated."
 
     # old code here.
   end
+  #:method end
 
+  #:test
   it "gives a deprecation warning" do
     _, err = capture_io do
       outdated_method()
@@ -15,4 +18,5 @@ class DocInternalsTest < Minitest::Spec
 
     assert_equal err, %([Trailblazer] #{__FILE__}:#{line_no - 2} The `#outdated_method` is deprecated.\n)
   end
+  #:test end
 end
