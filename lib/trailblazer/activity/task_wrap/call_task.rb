ff3b0598efce053cc11236f7f1035f3527fec874
Nick Sutterer <apotonick@gmail.com>
2025-04-09 11:25:16 +0200

    make the "call task" more implicit. We need it later.

--- lib/trailblazer/activity/task_wrap/call_task.rb
+++ lib/trailblazer/activity/task_wrap/call_task.rb
@@ -10,7 +10,7 @@ class Trailblazer::Activity
 
       # Call the actual task we're wrapping here.
       # puts "~~~~wrap.call: #{task}"
-      return_signal, return_args = task.(original_arguments, **original_circuit_options)
+      return_signal, return_args = task.call(original_arguments, **original_circuit_options)
 
       # DISCUSS: do we want original_args here to be passed on, or the "effective" return_args which are different to original_args now?
       wrap_ctx = wrap_ctx.merge(
