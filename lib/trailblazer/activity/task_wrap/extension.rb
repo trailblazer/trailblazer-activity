bc0c75549bcee753c85fdbca284baf1e9561c0af
Nick Sutterer <apotonick@gmail.com>
2025-04-09 11:36:01 +0200

    make WrapStatic code a bit better understandable.

--- lib/trailblazer/activity/task_wrap/extension.rb
+++ lib/trailblazer/activity/task_wrap/extension.rb
@@ -53,21 +53,26 @@ module Trailblazer
         end
 
         # Extension are used at compile-time with {wrap_static}, mostly with the {dsl} gem.
-        # {WrapStatic} extensions are called for setup through {Intermediate.config} at compile-time.
+        # At compile-time, {WrapStatic} extensions are compiled in {Sequence::Compiler}.
+        #
         # Each extension alters the activity's wrap_static taskWrap.
+        # {WrapStatic#call} returns the new "config" object with an updated config[:wrap_static] field.
         class WrapStatic
           def initialize(extension:)
             @extension = extension
           end
 
+          # DISCUSS: `wrap_static[task]` is too implicit, we're returning the default tw here set via Hash.new(). the initial tw should probably be generated explicitly via the DSL?
           def call(config:, task:, **)
-            # Add the extension's task(s) to the activity's {:wrap_static} taskWrap
-            # which is stored in the {:config} field.
-            wrap_static = config[:wrap_static]                 # the activity's {wrap_static}.
-            task_wrap   = wrap_static[task] # the "original" taskWrap for {task}.
+            wrap_static = config[:wrap_static]  # The activity's data structure keeping a map of {task => task_wrap}. This will be
+                                                # merged into {config}
+            task_wrap   = wrap_static[task]     # the "actual" taskWrap for {task}. Your steps will be added to that.
+
+            # Execute the extension, which adds steps to the tw.
+            new_task_wrap = @extension.(task_wrap)
 
             # Overwrite the original task_wrap:
-            wrap_static = wrap_static.merge(task => @extension.(task_wrap))
+            wrap_static = wrap_static.merge(task => new_task_wrap)
 
             # DISCUSS: does it make sense to test the immutable behavior here? What would be the worst outcome?
             config.merge(wrap_static: wrap_static) # Return new config hash. This needs to be immutable code!
