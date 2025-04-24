a2b789cb02cbfe63bb49001d431bc09c2a89553a
Nick Sutterer <apotonick@gmail.com>
2025-04-24 16:55:33 +0200

    move test.

--- test/friendly_interface_test.rb
+++ test/friendly_interface_test.rb
@@ -51,4 +51,45 @@ class FriendlyInterfaceTest < Minitest::Spec
 
     assert_equal task_wrap.inspect, %([["task_wrap.call_task", Object], ["my.class", Class], ["my.object", Module]])
   end
+
+   describe "Extension" do
+    def add_1(wrap_ctx, original_args)
+      ctx, = original_args[0]
+      ctx[:seq] << 1
+      return wrap_ctx, original_args # yay to mutable state. not.
+    end
+
+    it "deprecates {TaskWrap.WrapStatic}" do
+      adds = [
+        [method(:add_1), id: "user.add_1", prepend: "task_wrap.call_task"],
+        # [method(:add_2), id: "user.add_2", append:  "task_wrap.call_task"],
+      ]
+
+      ext = nil
+      _, warning = capture_io do
+        ext = TaskWrap::Extension.WrapStatic(*adds)
+      end
+      line_number_for_binary = __LINE__ - 2
+
+      # lines = warning.split("\n")
+      # lines[0] = lines[0][0..-5]+"." if lines[0] =~ /\d-\d+-\d/
+      # warning = lines.join("\n")
+
+      assert_equal warning, %{[Trailblazer] #{File.realpath(__FILE__)}:#{line_number_for_binary} Using `TaskWrap::Extension.WrapStatic()` is deprecated. Please use `TaskWrap.Extension()`.\n}
+      assert_equal ext.class, Trailblazer::Activity::TaskWrap::Extension
+      # DISCUSS: should we test if the extension is correct?
+    end
+
+    it "{Extension#call} accepts **options" do
+      adds = [
+        [Object, id: "user.add_1", prepend: nil],
+      ]
+
+      ext = TaskWrap::Extension(*adds)
+
+      task_wrap = ext.([], some: :options)
+
+      assert_equal task_wrap.inspect, %([["user.add_1", Object]])
+    end
+  end
 end
