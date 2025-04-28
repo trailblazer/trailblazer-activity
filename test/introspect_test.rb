a134fa2fffc034a9c73d2d36728bb0eefd8035fd
Nick Sutterer <apotonick@gmail.com>
2025-04-28 09:29:11 +0200

    fix introspect_test.

--- test/introspect_test.rb
+++ test/introspect_test.rb
@@ -15,6 +15,15 @@ class IntrospectionTest < Minitest::Spec
       nesting_tasks = top.tasks(flat_activity: middle_activity)
       activity = top.activity(tasks: nesting_tasks) # [a, [a, [b, c]]]
 
+      flat_activity = Fixtures.flat_activity
+
+      middle_tasks = Fixtures.default_tasks("b" => flat_activity)
+      middle_activity = Fixtures.flat_activity(tasks: middle_tasks)
+
+      tasks = Fixtures.default_tasks("b" => middle_activity)
+      activity = Fixtures.flat_activity(tasks: tasks)
+
+
       #@ find top-activity which returns a special Node.
       node, host_activity, graph = Trailblazer::Activity::Introspect.find_path(activity, [])
       assert_equal node.class, Trailblazer::Activity::Schema::Nodes::Attributes
@@ -22,15 +31,15 @@ class IntrospectionTest < Minitest::Spec
       assert_equal host_activity, Trailblazer::Activity::TaskWrap.container_activity_for(activity)
 
       #@ one element path
-      node, host_activity, graph = Trailblazer::Activity::Introspect.find_path(activity, ["a"])
+      node, host_activity, graph = Trailblazer::Activity::Introspect.find_path(activity, ["b"])
       assert_equal node.class, Trailblazer::Activity::Schema::Nodes::Attributes
-      assert_equal node[:task], Implementing.method(:a)
+      assert_equal node[:task], middle_activity
       assert_equal host_activity, activity
 
       #@ nested element
-      node, host_activity, _graph = Trailblazer::Activity::Introspect.find_path(activity, ["flat_activity", "flat_activity", "b"])
+      node, host_activity, _graph = Trailblazer::Activity::Introspect.find_path(activity, ["b", "b", "c"])
       assert_equal node.class, Trailblazer::Activity::Schema::Nodes::Attributes
-      assert_equal node[:task], Implementing.method(:b)
+      assert_equal node[:task], Implementing.method(:c)
       assert_equal host_activity, flat_activity
 
       #@ non-existent element
@@ -70,9 +79,12 @@ class IntrospectionTest < Minitest::Spec
     end
 
     it "accepts {id: nil}" do
+      flat_activity = Fixtures.flat_activity
+
       container_activity = Trailblazer::Activity::TaskWrap.container_activity_for(flat_activity)
 
       attrs = Trailblazer::Activity::Introspect.Nodes(container_activity, id: nil)
+
 
       assert_equal attrs.id, nil
       assert_equal attrs.task, flat_activity
