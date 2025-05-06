2bd776985cc52a05df3889fcef7b6a372f30c703
Nick Sutterer <apotonick@gmail.com>
2025-05-06 10:13:53 +0200

    test both ADDS APIs.

--- test/adds_test.rb
+++ test/adds_test.rb
@@ -8,6 +8,144 @@ class AddsTest < Minitest::Spec
 #@ No mutation on original pipe
 #@ Those tests are written in one single {it} on purpose. Further on, we perform all ADDS operations
 #@ before we assert the particular pipelines to test if anything gets mutated during the way.
+
+  # Canonical top-level API
+  it "what" do
+    pipe1 = pipeline.new([pipeline::Row["task_wrap.call_task", "task, call"]])
+
+  #@ {Prepend} to element 0
+    pipe2 = adds.(pipe1, ["trace, prepare", prepend: "task_wrap.call_task", id: "trace-in-outer"])
+
+  #@ {Append} to element 0
+    pipe3 = adds.(pipe2, ["trace, prepare", append: "task_wrap.call_task", id: "trace-out-outer"])
+
+  #@ {Prepend} again
+    pipe4 = adds.(pipe3, ["trace, prepare", prepend: "task_wrap.call_task", id: "trace-in-inner"])
+
+  #@ {Append} again
+    pipe5 = adds.(pipe4, ["trace, prepare", append: "task_wrap.call_task", id: "trace-out-inner"])
+
+  #@ {Append} to last element
+    pipe6 = adds.(pipe5, ["log", append: "trace-out-outer", id: "last-id"])
+
+  #@ {Replace} first element
+    pipe7 = adds.(pipe6, ["log", replace: "trace-in-outer", id: "first-element"])
+
+  #@ {Replace} last element
+    pipe8 = adds.(pipe7, ["log", replace: "last-id", id: "last-element"])
+
+  #@ {Replace} middle element
+    pipe9 = adds.(pipe8, ["log", replace: "trace-out-outer", id: "middle-element"])
+
+  #@ {Delete} first element
+    pipe10 = adds.(pipe9, ["log", delete: "first-element", id: nil])
+
+  #@ {Delete} last element
+    pipe11 = adds.(pipe10, ["log", delete: "last-element", id: nil])
+
+  #@ {Delete} middle element
+    pipe12 = adds.(pipe11, [nil, delete: "trace-out-inner", id: nil])
+
+    assert_equal inspect(pipe1), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=[["task_wrap.call_task", "task, call"]]>
+}
+
+    assert_equal inspect(pipe2), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["trace-in-outer", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"]]>
+}
+
+    assert_equal inspect(pipe3), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["trace-in-outer", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"],
+   ["trace-out-outer", "trace, prepare"]]>
+}
+
+    assert_equal inspect(pipe4), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["trace-in-outer", "trace, prepare"],
+   ["trace-in-inner", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"],
+   ["trace-out-outer", "trace, prepare"]]>
+}
+
+    assert_equal inspect(pipe5), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["trace-in-outer", "trace, prepare"],
+   ["trace-in-inner", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"],
+   ["trace-out-inner", "trace, prepare"],
+   ["trace-out-outer", "trace, prepare"]]>
+}
+
+    assert_equal inspect(pipe6), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["trace-in-outer", "trace, prepare"],
+   ["trace-in-inner", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"],
+   ["trace-out-inner", "trace, prepare"],
+   ["trace-out-outer", "trace, prepare"],
+   ["last-id", "log"]]>
+}
+
+    assert_equal inspect(pipe7), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["first-element", "log"],
+   ["trace-in-inner", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"],
+   ["trace-out-inner", "trace, prepare"],
+   ["trace-out-outer", "trace, prepare"],
+   ["last-id", "log"]]>
+}
+
+    assert_equal inspect(pipe8), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["first-element", "log"],
+   ["trace-in-inner", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"],
+   ["trace-out-inner", "trace, prepare"],
+   ["trace-out-outer", "trace, prepare"],
+   ["last-element", "log"]]>
+}
+
+    assert_equal inspect(pipe9), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["first-element", "log"],
+   ["trace-in-inner", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"],
+   ["trace-out-inner", "trace, prepare"],
+   ["middle-element", "log"],
+   ["last-element", "log"]]>
+}
+
+    assert_equal inspect(pipe10), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["trace-in-inner", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"],
+   ["trace-out-inner", "trace, prepare"],
+   ["middle-element", "log"],
+   ["last-element", "log"]]>
+}
+
+    assert_equal inspect(pipe11), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["trace-in-inner", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"],
+   ["trace-out-inner", "trace, prepare"],
+   ["middle-element", "log"]]>
+}
+
+    assert_equal inspect(pipe12), %{#<Trailblazer::Activity::TaskWrap::Pipeline:
+ @sequence=
+  [["trace-in-inner", "trace, prepare"],
+   ["task_wrap.call_task", "task, call"],
+   ["middle-element", "log"]]>
+}
+  end
+
+  # Internal API, currently used in dsl, too.
   it "what" do
     pipe1 = pipeline.new([pipeline::Row["task_wrap.call_task", "task, call"]])
 
