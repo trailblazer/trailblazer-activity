20ca1b31cebe2bae05618af6bbd80bb4c69de324
Nick Sutterer <apotonick@gmail.com>
2025-10-24 10:01:08 +0200

    better comments.

--- lib/trailblazer/activity/task_wrap/call_task.rb
+++ lib/trailblazer/activity/task_wrap/call_task.rb
@@ -1,12 +1,12 @@
 class Trailblazer::Activity
   module TaskWrap
-    # TaskWrap step that calls the actual wrapped task and passes all `original_args` to it.
+    # TaskWrap step that calls the actual wrapped task while passing approriate arguments.
     #
-    # It writes to wrap_ctx[:return_signal], wrap_ctx[:return_args]
+    # It writes to wrap_ctx[:return_signal], wrap_ctx[:return_ctx]
     def self.call_task(wrap_ctx, flow_options, _)
       task                        = wrap_ctx[:task]
       application_ctx             = wrap_ctx[:application_ctx]
-      application_circuit_options = wrap_ctx[:application_circuit_options]
+      application_circuit_options = wrap_ctx[:application_circuit_options] # FIXME: introduce kwargs in 2nd method.
 
       # Call the actual task we're wrapping here.
       # puts "~~~~wrap.call: #{task}"
@@ -17,13 +17,12 @@ class Trailblazer::Activity
           application_circuit_options # We maintain an alterable {circuit_options} in the taskWrap. This allows things such as changing {:start_task}.
         )
 
-      # DISCUSS: do we want original_args here to be passed on, or the "effective" return_args which are different to original_args now?
       wrap_ctx = wrap_ctx.merge(
         return_signal: return_signal,
-        return_ctx:   ctx
+        return_ctx:    ctx
       )
 
       return wrap_ctx, flow_options
     end
-  end # Wrap
+  end
 end
