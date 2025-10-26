5b85c587eb9c76a02876d060f17ff6afab3588ce
Nick Sutterer <apotonick@gmail.com>
2025-10-26 19:35:08 +0100

    there's only Circuit::Step now, removing TaskAdapter and all its weird friends. much simpler architecture now thanks to the new circuit interface. we can use the returned signal as a value in filters.

--- lib/trailblazer/activity/circuit/task_adapter.rb
+++ lib/trailblazer/activity/circuit/task_adapter.rb
@@ -1,5 +1,24 @@
 module Trailblazer
   class Activity
+    # Since we're always assuming filters exposing a circuit interface, this has been moved
+    # into the Activity namespace, yo.
+    #
+    # TODO: make Option a private concept, make devs use {Circuit.Step} instead.
+    class Option # FIXME: remove trailblazer-option.
+      def initialize(filter)
+        @filter = filter
+      end
+
+      # To invoke an instance_method, additional logic is needed, e.g. figuring out the {exec_context}.
+      #
+      # PROBLEM: we don't know the signature of the instance_method, is it a step, is it circuit interface?
+      class InstanceMethod < Option
+        def call(*args, keyword_arguments: {}, exec_context: raise("No :exec_context given."), **)
+          exec_context.send(@filter, *args, **keyword_arguments)
+        end
+      end
+    end
+
     # Circuit::TaskAdapter:   Uses Circuit::Step to translate incoming, and returns a circuit-interface
     #                 compatible return set.
     class Circuit
@@ -10,93 +29,107 @@ module Trailblazer
       # @param    [:option] If true, the user's callable argument is wrapped in `Trailblazer::Option`.
       # @return   [Circuit::Step, Circuit::Step::Option] Returns a callable circuit-step.
       # @see      https://trailblazer.to/2.1/docs/activity#activity-internals-step-interface
-      def self.Step(callable_with_step_interface, option: false)
-        if option
-          return Step::Option.new(Trailblazer::Option(callable_with_step_interface), callable_with_step_interface)
+      def self.Step(callable_with_step_interface, option: false, instance_method: false, binary: nil)
+
+        raise if option # FIXME: remove once this is sorted.
+
+        # This will result in a step being wrapped in a Binary step.
+        binary = Step::Binary if binary === true
+
+        circuit_step =
+          # TODO: should we detect here if Option wrapping is needed?
+          if instance_method
+            # currently, this is only used for {:instance_method} filters.
+            Step::Option.new(Activity::Option::InstanceMethod.new(callable_with_step_interface))
+          else
+            Step.new(callable_with_step_interface)
+          end
+
+        if binary
+          circuit_step = binary.new(circuit_step) # DISCUSS: i hate wrapping, but we cannot use an Activity-based approach, yet.
         end
 
-        Step.new(callable_with_step_interface, callable_with_step_interface) # DISCUSS: maybe we can ditch this, only if performance is cool here, but where would we use that?
+        return circuit_step
       end
 
       # {Step#call} translates the incoming circuit-interface to the step-interface,
-      # and returns the return value of the user's callable. By design, it is *not* circuit-interface compatible.
+      # and returns the return value of the user's callable.
       class Step
-        def initialize(step, user_proc, **)
-          @step      = step
-          @user_proc = user_proc
+        def initialize(step)
+          @step = step
         end
 
-        # Translate the circuit interface to the step's step-interface. However,
-        # this only translates the calling interface, not the returning.
-        def call(ctx, flow_options, **circuit_options)
-          result = @step.(ctx, **ctx.to_hash)
-          # in an immutable environment we should return the ctx from the step.
-          return result, ctx
+        # NOTE: step interface is (ctx, **kwargs) and returns a single value (the "signal").
+
+        # Translate the circuit interface to the step's step-interface.
+        # We treat the return value as a "signal".
+        def call(ctx, flow_options, circuit_options)
+          result = @step.(ctx, **ctx.to_hash) # call @step with step interface.
+
+          # DISCUSS: in an immutable environment we should return the ctx from the step.
+          # in an additional "layer" we could treat the result as something coming from return Result(ctx, Success|true),
+          # and then return the new ctx here. it's much more convenient letting OP steps mutate the ctx directly, though.
+
+          return ctx, flow_options, result
         end
 
         # In {Step::Option}, {@step} is expected to be wrapped in a {Trailblazer::Option}.
-        # To remember: when calling an Option instance, you need to pass {:keyword_arguments} explicitely,
+        # To remember: when calling an Option instance, you need to pass {:keyword_arguments} explicitly,
         # because of beautiful Ruby 2.5 and 2.6.
         #
         # This is often needed for "decider" chunks where the user can run either a method or a callable
         # but you only want back the return value, not a Binary circuit-interface return set.
         class Option < Step
           def call(ctx, flow_options, circuit_options)
+            # Invoke Option::InstanceMethod#call():
             result = @step.(ctx, keyword_arguments: ctx.to_hash, **circuit_options) # circuit_options contains :exec_context.
-            # in an immutable environment we should return the ctx from the step.
-            return result, ctx
-          end
-        end
-      end
 
-      class TaskAdapter
-        # Returns a `TaskAdapter` instance always exposes the complete circuit-interface,
-        # and can be used directly in a {Circuit}.
-        #
-        # @note     This used to be called `TaskBuilder::Task`.
-        # @param    [step] Any kind of callable object or `:instance_method` that receives
-        #   a step interface.
-        # @param    [:option] If true, the user's callable argument is wrapped in `Trailblazer::Option`.
-        # @return   [TaskAdapter] a circuit-interface compatible object to use in a `Circuit`.
-        def self.for_step(step, binary: true, **options_for_step)
-          circuit_step = Circuit.Step(step, **options_for_step)
-
-          new(circuit_step)
-        end
-
-        # @param [circuit_step] Exposes a Circuit::Step.call([ctx, flow_options], **circuit_options) interface
-        def initialize(circuit_step, **)
-          @circuit_step = circuit_step
-        end
-
-        def call(ctx, flow_options, circuit_options)
-          result, ctx = @circuit_step.(ctx, flow_options, circuit_options) # DISCUSS: do we want to return {flow_options} here?
-
-          # Return an appropriate signal which direction to go next.
-          signal = TaskAdapter.binary_signal_for(result, Activity::Right, Activity::Left)
-
-          return signal, ctx, flow_options
-        end
-
-        # Translates the return value of the user step into a valid signal.
-        # Note that it passes through subclasses of {Activity::Signal}.
-        def self.binary_signal_for(result, on_true, on_false)
-          if result.is_a?(Class) && result < Activity::Signal
-            result
-          else
-            result ? on_true : on_false
+            return ctx, flow_options, result
           end
         end
 
-        # FIXME: this method sucks as it shows the wrong name.
-        def inspect # TODO: make me private!
-          # return super
-          user_step = @circuit_step.instance_variable_get(:@user_proc) # DISCUSS: to we want Step#to_h?
+        # A Binary instance wraps a Step instance and adds logic for processing the result "signal".
+        class Binary < Step
+          def call(ctx, flow_options, circuit_options)
+            ctx, flow_options, result = @step.(ctx, flow_options, circuit_options)
 
-          %(#<Trailblazer::Activity::TaskBuilder::Task user_proc=#{Trailblazer::Activity::Introspect.render_task(user_step)}>)
+            # Return an appropriate signal which direction to go next.
+            ctx, flow_options, signal = compute_signal(ctx, flow_options, result)
+
+            return ctx, flow_options, signal
+          end
+
+          def compute_signal(ctx, flow_options, result)
+            signal = Binary.binary_signal_for(result, Activity::Right, Activity::Left)
+
+            return ctx, flow_options, signal
+          end
+
+          # Translates the return value of the user step into a valid signal.
+          # Note that it passes through subclasses of {Activity::Signal}.
+          def self.binary_signal_for(result, on_true, on_false)
+            if result.is_a?(Class) && result < Activity::Signal
+              result
+            else
+              result ? on_true : on_false
+            end
+          end
         end
-        alias_method :to_s, :inspect
-      end
+      end # Step
+
+      # FIXME:, docs: TaskAdapter instance wraps a user code chunk with a circuit interface, the return value of the user code is arbitrary and we translate it to a binary signal.
+
+      # FIXME: deprecate TaskAdapter
+
+      #   # FIXME: this method sucks as it shows the wrong name.
+      #   def inspect # TODO: make me private!
+      #     # return super
+      #     user_step = @circuit_step.instance_variable_get(:@user_proc) # DISCUSS: to we want Step#to_h?
+
+      #     %(#<Trailblazer::Activity::TaskBuilder::Task user_proc=#{Trailblazer::Activity::Introspect.render_task(user_step)}>)
+      #   end
+      #   alias_method :to_s, :inspect
+      # end
     end # Circuit
   end # Activity
 end
--- test/task_adapter_test.rb
+++ test/task_adapter_test.rb
@@ -9,81 +9,184 @@ class TaskAdapterTest < Minitest::Spec
     end
   end
 
-  it "Circuit::TaskAdapter" do
+  def my_output(ctx, params:, **)
+    {
+      id: params[:id],
+    }
+  end
 
+  def my_output_with_circuit_interface(ctx, flow_options, circuit_options)
+    value = {
+      id:           ctx[:params][:id],
+      exec_context: circuit_options[:exec_context],
+    }
 
-    # steps receive ctx and kwargs
-    # steps can write to something
-    # steps return an outcome "boolean"/return value
-    step = ->(ctx, model:, **) do
-      return if model.nil?
+    return ctx, flow_options, value
+  end
 
-      ctx[:type] = model.class
+  let(:ctx) { {params: {id: 1}, action: :update} }
+
+  it "Option::InstanceMethod" do
+  # instance method with step interface
+    option = Trailblazer::Activity::Option::InstanceMethod.new(:my_output)
+
+    circuit_options = {exec_context: self, wrap_runtime: {}}
+
+    value = option.(self.ctx, keyword_arguments: self.ctx.to_hash, **circuit_options)
+
+    assert_equal value, {id: 1}
+
+  # instance method with different interface, for example, circuit interface (how surprising!)
+    option = Trailblazer::Activity::Option::InstanceMethod.new(:my_output_with_circuit_interface)
+
+    ctx, flow_options, value = option.(self.ctx, flow_options, circuit_options, **circuit_options) # DISCUSS: omitting {:keyword_arguments} might lead to problems in Ruby < 2.7.
+
+    assert_equal value, {id: 1, exec_context: self}
+    assert_equal ctx, self.ctx
+  end
+
+  it "Circuit.Step" do
+  # callable with step interface
+    task_to_step = Trailblazer::Activity::Circuit.Step(method(:my_output))
+
+    # this is how it'd be executed in the Circuit.
+    ctx, flow_options, value = task_to_step.(self.ctx, {stack: []}, {exec_context: self})
+    assert_equal ctx, self.ctx
+    assert_equal flow_options, {stack: []}
+    assert_equal value, {id: 1}
+
+  # :instance_method with step interface
+    task_to_step = Trailblazer::Activity::Circuit.Step(:my_output, instance_method: true)
+
+    ctx, flow_options, value = task_to_step.(self.ctx, {stack: []}, {exec_context: self})
+    assert_equal ctx, self.ctx
+    assert_equal flow_options, {stack: []}
+    assert_equal value, {id: 1}
+  end
+
+  def my_output_decider(ctx, valid:, **)
+    ctx[:seq] << :my_output_decider
+
+    valid
+  end
+
+  describe "with binary wrapping" do
+    def ctx
+      {seq: [], valid: true}
     end
 
-    flow_options    = {}
-    circuit_options = {}
+# TODO: test that we can return {flow_options}.
 
-    ctx             = {model: nil}
+    it "with callable" do
+    # callable with step interface
+      step = Trailblazer::Activity::Circuit.Step(method(:my_output_decider), binary: true)
 
-  # execute step in a circuit-interface environment
-    circuit_step = Trailblazer::Activity::Circuit::Step(step) # circuit step receives circuit-interface but returns only value
-    return_value, ctx = circuit_step.([ctx, flow_options], **circuit_options)
+      # this is how it'd be executed in the Circuit.
+      ctx, flow_options, signal = step.(self.ctx, {stack: []}, {exec_context: self})
+      assert_equal CU.inspect(ctx), %({:seq=>[:my_output_decider], :valid=>true})
+      assert_equal flow_options, {stack: []}
+      assert_equal signal, Trailblazer::Activity::Right
 
-    assert_nil return_value
-    assert_equal CU.inspect(ctx), %{{:model=>nil}}
+      left_ctx = self.ctx().merge(valid: false)
 
+      ctx, flow_options, signal = step.(left_ctx, {stack: []}, {exec_context: self})
+      assert_equal CU.inspect(ctx), %({:seq=>[:my_output_decider], :valid=>false})
+      assert_equal flow_options, {stack: []}
+      assert_equal signal, Trailblazer::Activity::Left
+    end
 
-  #@ execute step-option in a circuit-interface environment
-    circuit_step = Trailblazer::Activity::Circuit.Step(step, option: true) # wrap step with Trailblazer::Option
-    return_value, ctx = circuit_step.([ctx, flow_options], **circuit_options, exec_context: self)
-    assert_nil return_value
-    assert_equal CU.inspect(ctx), %{{:model=>nil}}
+    it "{:instance_method} with step interface" do
+      step = Trailblazer::Activity::Circuit.Step(:my_output_decider, instance_method: true, binary: true)
 
-    circuit_step = Trailblazer::Activity::Circuit::Step(:process_type, option: true) # wrap step with Trailblazer::Option
-    return_value, ctx = circuit_step.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
-    assert_nil return_value
-    assert_equal CU.inspect(ctx), %{{:model=>nil}}
+      ctx, flow_options, signal = step.(self.ctx, {stack: []}, {exec_context: self})
+      assert_equal CU.inspect(ctx), %({:seq=>[:my_output_decider], :valid=>true})
+      assert_equal flow_options, {stack: []}
+      assert_equal signal, Trailblazer::Activity::Right
 
-  #@ circuit-interface Option-compatible Step that does a Binary signal decision
-    step          = :process_type
-    circuit_task  = Trailblazer::Activity::Circuit::TaskAdapter.for_step(step, option: true)
+      left_ctx = self.ctx.merge(valid: false)
 
-    ctx = {model: nil}
-    signal, (ctx, flow_options) = circuit_task.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
-
-    assert_equal signal, Trailblazer::Activity::Left
-    assert_equal CU.inspect(ctx), %{{:model=>nil}}
-    assert_equal flow_options.inspect, %{{}}
-
-    ctx = {model: Object}
-    signal, (ctx, flow_options) = circuit_task.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
-
-    assert_equal signal, Trailblazer::Activity::Right
-    assert_equal CU.inspect(ctx), %{{:model=>Object, :type=>Class}}
-    assert_equal flow_options.inspect, %{{}}
-
-    ctx = {model: nil}
-    signal, (ctx, flow_options) = circuit_task.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
-
-    assert_equal signal, Trailblazer::Activity::Left
-    assert_equal CU.inspect(ctx), %{{:model=>nil}}
-    assert_equal flow_options.inspect, %{{}}
-
-#@ pipeline-interface
-
-    # we don't have {:exec_context} in a Pipeline!
-    step = Operation.new.method(:process_type)
-    args = [1,2]
-    ctx  = {model: Object}
-
-    # FIXME: remove, this has been moved to DSL.
-    # pipeline_task = Trailblazer::Activity::TaskWrap::Pipeline::TaskAdapter.for_step(step) # Task receives circuit-interface but it's compatible with Pipeline interface
-    # wrap_ctx, args = pipeline_task.(ctx, args) # that's how pipeline tasks are called in {TaskWrap::Pipeline}.
-
-    # assert_equal CU.inspect(wrap_ctx), %{{:model=>Object, :type=>Class}}
-    # assert_equal args, [1,2]
+      ctx, flow_options, signal = step.(left_ctx, {stack: []}, {exec_context: self})
+      assert_equal CU.inspect(ctx), %({:seq=>[:my_output_decider], :valid=>false})
+      assert_equal flow_options, {stack: []}
+      assert_equal signal, Trailblazer::Activity::Left
+    end
   end
+
+#   it "Circuit::TaskAdapter" do
+
+
+#     # steps receive ctx and kwargs
+#     # steps can write to something
+#     # steps return an outcome "boolean"/return value
+#     step = ->(ctx, model:, **) do
+#       return if model.nil?
+
+#       ctx[:type] = model.class
+#     end
+
+#     flow_options    = {}
+#     circuit_options = {}
+
+#     ctx             = {model: nil}
+
+#   # execute step in a circuit-interface environment
+#     circuit_step = Trailblazer::Activity::Circuit::Step(step) # circuit step receives circuit-interface but returns only value
+#     return_value, ctx = circuit_step.([ctx, flow_options], **circuit_options)
+
+#     assert_nil return_value
+#     assert_equal CU.inspect(ctx), %{{:model=>nil}}
+
+
+#   #@ execute step-option in a circuit-interface environment
+#     circuit_step = Trailblazer::Activity::Circuit.Step(step, option: true) # wrap step with Trailblazer::Option
+#     return_value, ctx = circuit_step.([ctx, flow_options], **circuit_options, exec_context: self)
+#     assert_nil return_value
+#     assert_equal CU.inspect(ctx), %{{:model=>nil}}
+
+#     circuit_step = Trailblazer::Activity::Circuit::Step(:process_type, option: true) # wrap step with Trailblazer::Option
+#     return_value, ctx = circuit_step.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
+#     assert_nil return_value
+#     assert_equal CU.inspect(ctx), %{{:model=>nil}}
+
+#   #@ circuit-interface Option-compatible Step that does a Binary signal decision
+#     step          = :process_type
+#     circuit_task  = Trailblazer::Activity::Circuit::TaskAdapter.for_step(step, option: true)
+
+#     ctx = {model: nil}
+#     signal, (ctx, flow_options) = circuit_task.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
+
+#     assert_equal signal, Trailblazer::Activity::Left
+#     assert_equal CU.inspect(ctx), %{{:model=>nil}}
+#     assert_equal flow_options.inspect, %{{}}
+
+#     ctx = {model: Object}
+#     signal, (ctx, flow_options) = circuit_task.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
+
+#     assert_equal signal, Trailblazer::Activity::Right
+#     assert_equal CU.inspect(ctx), %{{:model=>Object, :type=>Class}}
+#     assert_equal flow_options.inspect, %{{}}
+
+#     ctx = {model: nil}
+#     signal, (ctx, flow_options) = circuit_task.([ctx, flow_options], **circuit_options, exec_context: Operation.new)
+
+#     assert_equal signal, Trailblazer::Activity::Left
+#     assert_equal CU.inspect(ctx), %{{:model=>nil}}
+#     assert_equal flow_options.inspect, %{{}}
+
+# #@ pipeline-interface
+
+#     # we don't have {:exec_context} in a Pipeline!
+#     step = Operation.new.method(:process_type)
+#     args = [1,2]
+#     ctx  = {model: Object}
+
+#     # FIXME: remove, this has been moved to DSL.
+#     # pipeline_task = Trailblazer::Activity::TaskWrap::Pipeline::TaskAdapter.for_step(step) # Task receives circuit-interface but it's compatible with Pipeline interface
+#     # wrap_ctx, args = pipeline_task.(ctx, args) # that's how pipeline tasks are called in {TaskWrap::Pipeline}.
+
+#     # assert_equal CU.inspect(wrap_ctx), %{{:model=>Object, :type=>Class}}
+#     # assert_equal args, [1,2]
+#   end
 
   # TODO: properly test {TaskAdapter#inspect}.
 
