a419ddfb4c55eac56d124315e0915e232ee3de7b
Nick Sutterer <apotonick@gmail.com>
2025-11-26 17:49:24 +0100

    fix Ruby 2.5 and 2.6.

--- lib/trailblazer/activity/option.rb
+++ lib/trailblazer/activity/option.rb
@@ -13,10 +13,23 @@ module Trailblazer
       #
       # PROBLEM: we don't know the signature of the instance_method, is it a step, is it circuit interface?
       class InstanceMethod < Option
-        def call(*args, keyword_arguments: {}, exec_context: raise("No :exec_context given."), **)
+        def call(*args, keyword_arguments: {}, exec_context:, **)
           exec_context.send(@filter, *args, **keyword_arguments)
+        end
+
+        module Ruby2_5_and_2_6 # TODO: remove once we drop Ruby < 2.7
+          def call(*args, keyword_arguments: nil, exec_context: raise("No :exec_context given."), **kws)
+            # Don't pass empty `keyword_arguments` because Ruby <= 2.6 passes an empty hash for `**{}`
+            return exec_context.send(@filter, *args) unless keyword_arguments
+
+            super(*args, keyword_arguments: keyword_arguments, exec_context: exec_context, **kws)
+          end
         end
       end
     end
   end
+end
+
+if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.7.0')
+  Trailblazer::Activity::Option::InstanceMethod.prepend Trailblazer::Activity::Option::InstanceMethod::Ruby2_5_and_2_6
 end
