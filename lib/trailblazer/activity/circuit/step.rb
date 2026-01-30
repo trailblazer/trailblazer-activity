1acd2f17a69b97a2e5c40398fc521a6bb9a8788b
Nick Sutterer <apotonick@gmail.com>
2026-01-30 15:43:54 +0100

    removing all "old" code for Step.

--- lib/trailblazer/activity/circuit/step.rb
+++ lib/trailblazer/activity/circuit/step.rb
@@ -47,13 +47,6 @@ module Trailblazer
           end
         end # Generic
 
-
-
-
-
-        # class InstanceMethod  #< Activity::Railway
-          # step # DISCUSS: whoopsi, we don't have the DSL here :D
-        # end
         InstanceMethod = Activity.Pipeline(
           compute_callable: Generic::InstanceMethod.method(:compute_callable),
           invoke_callable: Generic.method(:invoke_callable), # FIXME: is this generic or Task-specific? we don't use it for Step?
@@ -208,134 +201,11 @@ module Trailblazer
             {} # library_ctx
           )
         end
-
-
-
-
-
-
-        # # def self.invoke_callable_with_step_interface(callable, ctx, flow_options, circuit_options)
-        # # Generic #call that invokes callable with step interface.
-        # def call(ctx, flow_options, circuit_options)
-        #   _result = user_filter.(ctx, **ctx.to_h) # This is how any Step should be called!
-        # end
-
-        class InstanceMethod < Struct.new(:generic_instance_method_caller)
-          def call(ctx, flow_options, circuit_options) # FIXME: applies only to {:instance_method}
-            callable = generic_instance_method_caller.(ctx, flow_options, circuit_options) # DISCUSS: copied from {Task#call}.
-
-            # Problem: we only know the callable at runtime!
-            # We also don't want to expose private affairs of the Step implementation here, here we
-            # want to use its public API.
-            step = Step___.new(callable) # FIXME: using the Wrapping approach shows its flaws here.
-            result = step.(ctx, flow_options, circuit_options)
-
-            return result
-          end
-        end
       end
 
-      # {Step#call} translates the incoming circuit-interface to the step-interface,
-      # and returns the return value of the user's callable.
-      class Step
-        def self.build(callable_with_step_interface, option: false, instance_method: false, binary: nil)
-          # This will result in a step being wrapped in a Binary step.
-          binary = Step::Binary if binary === true
-
-          circuit_step =
-            # TODO: should we detect here if Option wrapping is needed?
-            if instance_method
-              # currently, this is only used for {:instance_method} filters.
-              Step::Option.new(Activity::Option::InstanceMethod.new(callable_with_step_interface))
-            else
-              Step.new(callable_with_step_interface)
-            end
-
-          if binary
-            circuit_step = binary.new(circuit_step) # DISCUSS: i hate wrapping, but we cannot use an Activity-based approach, yet.
-          end
-
-          return circuit_step
-        end
-
-        def initialize(step)
-          @step = step
-        end
-
-        # NOTE: step interface is (ctx, **kwargs) and returns a single value (the "signal").
-
-        # Translate the circuit interface to the step's step-interface.
-        # We treat the return value as a "signal".
-        def call(ctx, flow_options, circuit_options)
-          result = @step.(ctx, **ctx.to_hash) # call @step with step interface.
-
-          # DISCUSS: in an immutable environment we should return the ctx from the step.
-          # in an additional "layer" we could treat the result as something coming from return Result(ctx, Success|true),
-          # and then return the new ctx here. it's much more convenient letting OP steps alter the ctx directly, though.
-
-          return ctx, flow_options, result
-        end
-
-        # DISCUSS: here, we have knowledge about how an Option::InstanceMethod is being called, and what it
-        #          returns. here, we assume that it's always one particular value. what if we want to call a @step with a circuit interface?
-        #
-        # In {Step::Option}, {@step} is expected to be wrapped in a {Trailblazer::Option}.
-        # To remember: when calling an Option instance, you need to pass {:keyword_arguments} explicitly,
-        # because of beautiful Ruby 2.5 and 2.6.
-        #
-        # This is often needed for "decider" chunks where the user can run either a method or a callable
-        # but you only want back the return value, not a Binary circuit-interface return set.
-        class Option < Step
-          def call(ctx, flow_options, circuit_options)
-            # Invoke Option::InstanceMethod#call():
-            result = @step.(ctx, keyword_arguments: ctx.to_hash, **circuit_options) # {circuit_options} contain {:exec_context}.
-
-            return ctx, flow_options, result
-          end
-        end
-
-        # A Binary instance wraps a Step instance and adds logic for processing the result "signal".
-        class Binary < Step
-          def call(ctx, flow_options, circuit_options)
-            ctx, flow_options, result = @step.(ctx, flow_options, circuit_options)
-
-            # Return an appropriate signal which direction to go next.
-            ctx, flow_options, signal = compute_signal(ctx, flow_options, result)
-
-            return ctx, flow_options, signal
-          end
-
-          def compute_signal(ctx, flow_options, result)
-            signal = Binary.binary_signal_for(result, Activity::Right, Activity::Left)
-
-            return ctx, flow_options, signal
-          end
-
-          # Translates the return value of the user step into a valid signal.
-          # Note that it passes through subclasses of {Activity::Signal}.
-          def self.binary_signal_for(result, on_true, on_false)
-            if result.is_a?(Class) && result < Activity::Signal
-              result
-            else
-              result ? on_true : on_false
-            end
-          end
-        end
-      end # Step
 
       # FIXME:, docs: TaskAdapter instance wraps a user code chunk with a circuit interface, the return value of the user code is arbitrary and we translate it to a binary signal.
 
-      # FIXME: deprecate TaskAdapter
-
-      #   # FIXME: this method sucks as it shows the wrong name.
-      #   def inspect # TODO: make me private!
-      #     # return super
-      #     user_step = @circuit_step.instance_variable_get(:@user_proc) # DISCUSS: to we want Step#to_h?
-
-      #     %(#<Trailblazer::Activity::TaskBuilder::Task user_proc=#{Trailblazer::Activity::Introspect.render_task(user_step)}>)
-      #   end
-      #   alias_method :to_s, :inspect
-      # end
     end # Circuit
   end # Activity
 end
