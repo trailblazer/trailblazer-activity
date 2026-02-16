39396533329634639680b0023357d29689b0cf55
Nick Sutterer <apotonick@gmail.com>
2026-02-16 11:31:44 +0100

    comments.

--- lib/trailblazer/activity/circuit.rb
+++ lib/trailblazer/activity/circuit.rb
@@ -5,7 +5,7 @@ module Trailblazer
       # Find the next step for {current_task_id => signal}.
       # This is called in {Circuit::Processor.call}.
       def resolve(current_task_id, signal)
-        return if termini.include?(current_task_id)
+        return if termini.include?(current_task_id) # this is faster than any other trick I tried, with {terminus => nil} etc.
 
         signal_map = map[current_task_id] # assumption: ID must always be a symbol.
 # puts "@@@@@ #{current_task_id.inspect}, #{signal_map}"
@@ -13,8 +13,10 @@ module Trailblazer
 
         next_task_id = signal_map[signal] or raise signal.inspect # this will be nil for a terminus.
 
-        config[next_task_id]
+        config[next_task_id] # TODO: can we save this lookup and optimize the map directly?
       end
+
+
 
       # def start_for
       #   return termini, *config[start_task_id]
