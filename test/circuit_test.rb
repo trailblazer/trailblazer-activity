94d5f298248d525185f55fb9c19bd1fc556cab75
Nick Sutterer <apotonick@gmail.com>
2026-02-16 15:48:42 +0100

    adding a very simple Each implementation.

--- test/circuit_test.rb
+++ test/circuit_test.rb
@@ -1,5 +1,90 @@
 require "test_helper"
 
+class Circuit_dynamicResolving_for_Each_Test < Minitest::Spec
+  def my_task_a(ctx, value:, index:, **)
+    ctx[:seq] << [index, value]
+
+    return ctx, nil
+  end
+
+  class MyDynamicCircuit < Struct.new(:config, :map, keyword_init: true)
+    def to_a_FIXME
+      config[:init] # FIXME: what if dataset is empty?
+    end
+
+    def resolve(current_task_id, signal)
+      if current_task_id == :a_____________
+
+      else
+        config[map[current_task_id][signal]] # DISCUSS: original Circuit#resolve logic.
+
+      end
+    end
+  end
+
+  class MyEach
+    def self.init(ctx, dataset:, **)
+      return ctx.merge(
+        index: 0,
+        last_index: dataset.size,
+        ), nil
+    end
+
+    def self.fetch_value_from_dataset(ctx, index:, dataset:, **)
+      value = dataset[index]
+
+      return ctx.merge(value: value), nil
+    end
+
+    def self.increase_index(ctx, index:, dataset:, last_index:, **)
+      index += 1
+      return ctx, "done" if index == last_index
+      return ctx.merge(index: index)
+    end
+
+    def self.finished(ctx, **)
+      return ctx, nil
+    end
+  end
+
+  it do
+    config = {
+      init: [:init, :init, Trailblazer::Activity::Task::Invoker::CircuitInterface::InstanceMethod, {exec_context: MyEach}],
+      fetch_value_from_dataset: [:fetch_value_from_dataset, :fetch_value_from_dataset, Trailblazer::Activity::Task::Invoker::CircuitInterface::InstanceMethod, {exec_context: MyEach}],
+      a: [:a, :my_task_a, Trailblazer::Activity::Task::Invoker::CircuitInterface::InstanceMethod, {}],
+      increase_index: [:increase_index, :increase_index, Trailblazer::Activity::Task::Invoker::CircuitInterface::InstanceMethod, {exec_context: MyEach}],
+      finished: [:finished, :finished, Trailblazer::Activity::Task::Invoker::CircuitInterface::InstanceMethod, {exec_context: MyEach}],
+    }
+
+    map = {
+        init: {nil => :fetch_value_from_dataset},
+        fetch_value_from_dataset: {nil => :a},
+        a: {nil => :increase_index},
+        increase_index: {nil => :fetch_value_from_dataset, "done" => :finished},
+        finished: {}
+      }
+
+    circuit = MyDynamicCircuit.new(
+      # map:        map,
+      # start_task_id: :a,
+      # termini:    [:e],
+      config:     config,
+      map: map,
+    )
+
+    ctx, signal = Trailblazer::Activity::Circuit::Processor.(
+      circuit,
+      {seq: [], dataset: [1,2,3]},
+
+      exec_context: self # applies to all the pipeline's steps
+    )
+
+    assert_equal ctx[:seq], [[0, 1], [1, 2], [2, 3]]
+  end
+
+
+end
+
 # Here, we play with a "pipeline circuit" concept where there's no signal needed.
 class Circuit_FasterResolving_Test < Minitest::Spec
   def my_task_a(ctx, **)
