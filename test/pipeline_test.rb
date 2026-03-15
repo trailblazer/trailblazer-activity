07e1a5c20aa38e4c47ec2260cf85dfb32e88a547
Nick Sutterer <apotonick@gmail.com>
2026-03-15 21:09:07 +0100

    test Pipeline with Scoped nodes.

--- test/pipeline_test.rb
+++ test/pipeline_test.rb
@@ -8,6 +8,7 @@ class PipelineTest < Minitest::Spec
       return lib_ctx, flow_options, return_signal
     end
   end
+
   it "always uses {nil} as the lookup signal, regardless what was returned, but returns the actual last signal" do
     pipe = _A::Circuit::Builder.Pipeline(
       [:a, Task.new(:a, Object), _A::Circuit::Task::Adapter::LibInterface],
@@ -16,5 +17,22 @@ class PipelineTest < Minitest::Spec
     )
 
     assert_run pipe, terminus: Module, seq: [:a, :b, :c]
+  end
+
+  it "obviously allows scoping its elements" do
+    pipe = _A::Circuit::Builder.Pipeline(
+      [:a, Capture.new(:a), _A::Circuit::Task::Adapter::LibInterface, {}, _A::Circuit::Node::Scoped], # isolated.
+      [:b, Capture.new(:b), _A::Circuit::Task::Adapter::LibInterface, {}, _A::Circuit::Node::Scoped, copy_to_outer_ctx: [:d], merge_to_lib_ctx: {d: 4}],
+      [:c, Capture.new(:c), _A::Circuit::Task::Adapter::LibInterface, {}, _A::Circuit::Node::Scoped],
+    )
+
+    lib_ctx, flow_options = assert_run pipe, terminus: nil, seq: []
+    assert_equal flow_options, {
+      application_ctx: {:seq=>[]},
+
+      :a=> a = [{}, {:application_ctx=>{:seq=>[]}}, nil, {}],
+      :b=> b = [{:d=>4}, {:application_ctx=>{:seq=>[]}, a: a}, nil, {:d=>4}],
+      :c=> c = [{:d=>4}, {:application_ctx=>{:seq=>[]}, a: a, b: b}, nil, {:d=>4}],
+    }
   end
 end
