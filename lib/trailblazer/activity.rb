aa19916ee5d2090130240ae30f35515bde6d521a
Nick Sutterer <apotonick@gmail.com>
2026-04-02 15:58:03 +0200

    use circuit gem.

--- lib/trailblazer/activity.rb
+++ lib/trailblazer/activity.rb
@@ -6,19 +6,8 @@ module Trailblazer
   end # Activity
 end
 
-require "trailblazer/activity/circuit"
-require "trailblazer/activity/circuit/context"
-require "trailblazer/activity/circuit/node"
-require "trailblazer/activity/circuit/node/scoped"
-require "trailblazer/activity/circuit/node/runner"
-require "trailblazer/activity/circuit/node/introspect"
-require "trailblazer/activity/circuit/pipeline"
-require "trailblazer/activity/circuit/processor"
-require "trailblazer/activity/circuit/task/adapter"
-require "trailblazer/activity/circuit/builder"
-require "trailblazer/activity/circuit/adds"
-require "trailblazer/activity/circuit/wrap_runtime/runner"
-require "trailblazer/activity/circuit/wrap_runtime/extension"
+
+require "trailblazer/circuit"
 require "trailblazer/activity/signal"
 require "trailblazer/activity/terminus"
 require "trailblazer/activity/step" # ComputeBinarySignal.
