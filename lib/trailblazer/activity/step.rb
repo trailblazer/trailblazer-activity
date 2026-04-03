3de5884f8a3cec767c5d3ec8a6c2064ca9d695b2
Nick Sutterer <apotonick@gmail.com>
2026-04-03 08:28:01 +0200

    Step.build is now returning a node. more tests, eg for binary: false.

--- lib/trailblazer/activity/step.rb
+++ lib/trailblazer/activity/step.rb
@@ -1,14 +1,33 @@
 module Trailblazer
   class Activity
     module Step
-      # DISCUSS: should this be an instance method, too?
-      class ComputeBinarySignal
-        # Lib interface.
-        def self.call(lib_ctx, flow_options, signal, value:, **)
-          signal = value ? Trailblazer::Activity::Right : Trailblazer::Activity::Left
+      def self.build_pipeline(provider, binary:)
+        adapter = provider.is_a?(Symbol) ? Circuit::Task::Adapter::StepInterface::InstanceMethod : Circuit::Task::Adapter::StepInterface
 
-          return lib_ctx, flow_options, signal
-        end
+        steps = [
+          [:set_target_ctx, Step.method(:set_target_ctx)], # DISCUSS: the target_ctx related steps might be changed. They're currently the cleanest way to "configure" {invoke_provider}.
+          [:invoke_provider, provider, adapter],
+
+          # this step isn't necessary because a step, per definition, mutates the target_ctx. This is the
+          # opposite of clean, but it's the API we introduced and that proved to be super handy.
+          # [:unset_target_ctx, Step.method(:unset_target_ctx)], # write the mutated target_ctx back to where it came from originally.
+        ]
+
+        steps << [:compute_binary_signal, Step.method(:compute_binary_signal)] if binary
+
+        my_pipe = Trailblazer::Circuit::Builder.Pipeline(*steps)
+      end
+
+      def self.build(provider, id: :invoke_step, binary: true, **options_for_node)
+        pipe = build_pipeline(provider, binary: binary)
+
+        Circuit::Node::Scoped[id, pipe, Circuit::Processor, **options_for_node]
+      end
+
+      def self.compute_binary_signal(lib_ctx, flow_options, signal, value:, **)
+        signal = value ? Trailblazer::Activity::Right : Trailblazer::Activity::Left
+
+        return lib_ctx, flow_options, signal
       end
 
       def self.set_target_ctx(lib_ctx, flow_options, signal, **)
@@ -17,6 +36,8 @@ module Trailblazer
         return lib_ctx, flow_options, signal
       end
 
+      # In a world where the step interface mutates the target_ctx, we actually don't "need"
+      # this step (in a bad way, mutation sucks).
       def self.unset_target_ctx(lib_ctx, flow_options, signal, target_ctx:, **)
         flow_options = flow_options.merge(application_ctx: target_ctx)
 
--- test/step_test.rb
+++ test/step_test.rb
@@ -3,19 +3,54 @@ require_relative "test_helper"
 class StepTest < Minitest::Spec
   Step = Trailblazer::Activity::Step
 
-  it "doesn't rely on {application_ctx} mutability and writes the {target_ctx} back to {flow_options}" do
-    my_exec_context = T.def_steps(:a)
+  let(:my_exec_context) { T.def_steps(:a) }
 
-    my_pipe = Trailblazer::Circuit::Builder.Pipeline(
-      [:set_target_ctx, Step.method(:set_target_ctx)], # DISCUSS: the target_ctx related steps might be changed. They're currently the cleanest way to "configure" {invoke_provider}.
-      [:invoke_provider, :a, Trailblazer::Circuit::Task::Adapter::StepInterface::InstanceMethod],
-      [:unset_target_ctx, Step.method(:unset_target_ctx)], # write the mutated target_ctx back to where it came from originally.
-      [:compute_binary_signal, Step::ComputeBinarySignal],
-    )
+  it "uses flow_options[:application_ctx] as target_ctx and returns a binary signal" do
+    my_node = Trailblazer::Activity::Step.build(:a)
 
-    lib_ctx, flow_options, signal = assert_run my_pipe, seq: [:a],
+    lib_ctx, flow_options, signal = assert_run my_node, node: true, seq: [:a],
       exec_context: my_exec_context,
       terminus: Trailblazer::Activity::Right
+
+    lib_ctx, flow_options, signal = assert_run my_node, node: true, seq: [:a],
+      exec_context: my_exec_context,
+      flow_options: {application_ctx: {seq: [], a: false}},
+      terminus: Trailblazer::Activity::Left
+  end
+
+  it "can invoke callables" do
+    my_node = Trailblazer::Activity::Step.build(my_exec_context.method(:a))
+
+    lib_ctx, flow_options, signal = assert_run my_node, node: true, seq: [:a],
+      exec_context: my_exec_context,
+      terminus: Trailblazer::Activity::Right
+
+    lib_ctx, flow_options, signal = assert_run my_node, node: true, seq: [:a],
+      exec_context: my_exec_context,
+      flow_options: {application_ctx: {seq: [], a: false}},
+      terminus: Trailblazer::Activity::Left
+  end
+
+  it "{binary: false} only sets {:value} on internal {lib_ctx}" do
+    my_exec_context = Class.new do
+      def a(ctx, seq:, **)
+        seq << :a
+
+        {my_value: Hash}
+      end
+    end.new
+
+    my_node = Trailblazer::Activity::Step.build(:a, binary: false, copy_to_outer_ctx: [:value])
+
+    lib_ctx, flow_options, signal = assert_run my_node, node: true, seq: [:a],
+      exec_context: my_exec_context,
+      terminus: nil
+
+    assert_equal lib_ctx, {exec_context: my_exec_context, value: {my_value: Hash}}
+  end
+
+  it "doesn't rely on {application_ctx} mutability and writes the {target_ctx} back to {flow_options}" do
+
   end
 end
 
