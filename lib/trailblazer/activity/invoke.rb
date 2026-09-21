3b08bec170adbc2b94fb53f9f5b185bfd5c31819
Nick Sutterer <apotonick@gmail.com>
2026-09-21 17:16:44 +0200

    Invoke is now based on a Pipeline, so third party extensions can add their wisdom using the usual Adds interface. currently, we pass the "effective" circuit_options as the signal, works pretty well but the signature could be nicer. on the other hand, it's just a bunch of steps no one will ever really use.

--- lib/trailblazer/activity/invoke.rb
+++ lib/trailblazer/activity/invoke.rb
@@ -15,53 +15,37 @@ module Trailblazer
         return lib_ctx.merge(lib_ctx: lib_ctx_for_invoke)
       end
 
-      Options = Circuit::Builder.Pipeline(
-        [:compute_id, method(:compute_id)],
-        [:produce_lib_ctx, method(:produce_lib_ctx)],
-      )
-
-
-
       # The canonical invoke. Usually used by the user when doing Operation.().
-      def call(circuit, lib_ctx, options_compiler: Options, runner: Circuit::WrapRuntime::Runner, flow_options: {}, **circuit_options)
-        # TODO: make this a pipe?
-        # options_ctx = {target_ctx: target_ctx, node: activity_node, flow_options: {}, signal: nil}
+      def call(circuit, lib_ctx, compiler: Args::Compiler, runner: Circuit::WrapRuntime::Runner, flow_options: {}, **circuit_options)
+        circuit_options = circuit_options.merge(circuit: circuit)
 
-        # DISCUSS: in Processor, we don't allow passing on circuit_options. however, it's so cute an simple to just pass those
-        #          arguments onwards.
-        lib_ctx, flow_options, signal, circuit_options = build_activity_node(lib_ctx, flow_options, nil, runner: runner, circuit: circuit, **circuit_options)
-        lib_ctx, flow_options, signal, circuit_options = build_canonical_node(lib_ctx, flow_options, signal, **circuit_options)
-
-        # in canonical invoke, this could be part of the pipe.
-        # lib_ctx, flow_options, signal, circuit_options = _FIXME_add_options_for_trace(lib_ctx, flow_options, signal, **circuit_options)
-
-        # this is now specific to WrapRuntime
-        lib_ctx, flow_options, signal, circuit_options = produce_wrap_runtime(lib_ctx, flow_options, signal, **circuit_options)
-
-
-# TODO: add that in the canonical invoke
-        # options_ctx, _ = Circuit::Processor.(options_compiler, options_ctx, {}, nil, runner: Circuit::Node::Runner)
-
+        lib_ctx, flow_options, circuit_options = Circuit::Processor.(compiler, lib_ctx, flow_options, circuit_options, runner: Circuit::Node::Runner)
 
 
 
         # raise options_ctx.inspect
+        signal = nil
 
         invoke_runner(
           lib_ctx, flow_options, signal,
           # FIXME: where to do this?
           # wrap_runtime: Circuit::WrapRuntime::Extension::NodeWrap::Resolver.new(my_extensions),
+          runner: runner,
           **circuit_options
         )
       end
 
-      def build_activity_node(*args, circuit:, **circuit_options)
+      def build_activity_node(lib_ctx, flow_options, circuit_options, **)
+        circuit = circuit_options.fetch(:circuit)
+
         activity_node = Circuit::Node[circuit, Circuit::Processor] # DISCUSS: should that be done on the outside? should WTF pass the Node class here?
 
-        return *args, circuit_options.merge(node: activity_node)
+        return lib_ctx, flow_options, circuit_options.merge(node: activity_node)
       end
 
-      def build_canonical_node(*args, node:, **circuit_options)
+      def build_canonical_node(lib_ctx, flow_options, circuit_options, **)
+        node = circuit_options.fetch(:node)
+
         # build the canonical pipeline.
         canonical_pipeline = Circuit::Builder.Pipeline(
           [:"task_wrap.call_task", node: node]
@@ -71,22 +55,24 @@ module Trailblazer
         # TODO: allow mixing in task_wrap_extensions a la Subprocess from the "circuit"
         node = Circuit::Node[canonical_pipeline, Circuit::Processor]
 
-        return *args, circuit_options.merge(node: node)
+        return lib_ctx, flow_options, circuit_options.merge(node: node) # DISCUSS: introduce Adapter::Invoke ?
       end
 
       # DISCUSS: location?
       # DISCUSS: this is generic for the WrapRuntime layer, not for trace/wtf, only.
-      def produce_wrap_runtime(*args, extensions:, **circuit_options)
+      def produce_wrap_runtime(lib_ctx, flow_options, circuit_options, **)
+        extensions = circuit_options.fetch(:extensions)
+
         extensions = Circuit::WrapRuntime::Extension::Set.new(
           [
-            Circuit::WrapRuntime::Extension::NodeWrap,
+            Circuit::WrapRuntime::Extension::NodeWrap, # TODO: don't add that when we don't have any extensions!
             *extensions,
           ]
         )
 
         wrap_runtime = Circuit::WrapRuntime::Extension::NodeWrap::Resolver.new(extensions)
 
-        return *args, circuit_options.merge(wrap_runtime: wrap_runtime) # NOTE: we don't pass on {:extensions} here.
+        return lib_ctx, flow_options, circuit_options.merge(wrap_runtime: wrap_runtime) # NOTE: we don't pass on {:extensions} here.
       end
 
       def invoke_runner(lib_ctx, flow_options, signal, runner:, **circuit_options)
@@ -100,29 +86,19 @@ module Trailblazer
         )
       end
 
-      # def FIXME_additionals
-      #   # my_wtf_circuit_fixme = Trailblazer::Circuit::Builder.Pipeline(
-      #   #   [:wtf_top_canonical, node: my_canonical_Create_tw_node]
-      #   # )
-      #   # my_wtf_node = Trailblazer::Developer::Wtf::Node[my_wtf_circuit_fixme, Trailblazer::Circuit::Processor]
-
-      #   # DISCUSS: how to merge multiple runtime extensions? canonical invoke!
-      #   my_tracing_extension = Trailblazer::Circuit::WrapRuntime.Extension(adds: Trailblazer::Developer::Trace::Extension) # WrapRuntime::Extension means we adds
-
-      #   my_extensions = Trailblazer::Circuit::WrapRuntime::Extension::Set.new(
-      #     [
-      #       Trailblazer::Circuit::WrapRuntime::Extension::NodeWrap,
-      #       my_tracing_extension,
-      #     ]
-      #   )
-
-      #   flow_options = {
-      #     stack:              Trailblazer::Developer::Trace::Stack.new,
-      #     value_snapshooter:  Trailblazer::Developer::Trace.value_snapshooter
-      #   }
-
-      #   wrap_runtime: Trailblazer::Circuit::WrapRuntime::Extension::NodeWrap::Resolver.new(my_extensions),
-      # end
+      module Args
+        Compiler = Circuit::Builder.Pipeline(
+          # [:compute_id, method(:compute_id)],
+          # [:produce_lib_ctx, method(:produce_lib_ctx)],
+          [:build_activity_node, Invoke.method(:build_activity_node)],
+          [:build_canonical_node, Invoke.method(:build_canonical_node)],
+          # in canonical invoke, this could be part of the pipe.
+          # lib_ctx, flow_options, signal, circuit_options = _FIXME_add_options_for_trace(lib_ctx, flow_options, signal, **circuit_options)
+          #
+          # this is now specific to WrapRuntime
+          [:produce_wrap_runtime, Invoke.method(:produce_wrap_runtime)],
+        )
+      end
     end
   end
 end
--- test/invoke_test.rb
+++ test/invoke_test.rb
@@ -1,19 +1,38 @@
 require "test_helper"
 
 class InvokeTest < Minitest::Spec
-  it "what" do
-    my_circuit = Trailblazer::Circuit::Builder.Pipeline(
+  let(:my_circuit) do
+    Trailblazer::Circuit::Builder.Pipeline(
       [:a, T.def_tasks(:a).method(:a)],
     )
+  end
 
+  it "the default compiler only compiles a {:wrap_runtime} (TODO: test that we don't use it if no extensions are passed?)" do
     assert_run my_circuit, seq: [:a], terminus: Trailblazer::Activity::Right
 
-    Trailblazer::Activity::Invoke.(my_circuit, {target_ctx: {seq: []}}, extensions: [], id: :Create)
+    # wtf? can inject its extensions here, but no "global" canonical behavior.
+    lib_ctx, flow_options, signal = Trailblazer::Activity::Invoke.(my_circuit, {target_ctx: {seq: []}}, extensions: [], id: :Create)
+
+    assert_equal signal, Trailblazer::Activity::Right
+    assert_equal lib_ctx[:target_ctx][:seq], [:a]
 
     # we want
     # 1. call Circuit/node without having to remember all the flow_options, canonical node etc. use in #assert_run
     # 2. #wtf? can "inject" its pieces into the canonical invoke that might have other parts
     # 3. we can run Wtf.() without the canonical invoke.
     # 4.
+  end
+
+  it "we can use a canonical pipe that might have preconfigured steps (in a more or less global way)" do
+    # wtf? can inject its extensions here, but no "global" canonical behavior.
+    lib_ctx, flow_options, signal = Trailblazer::Activity::Invoke.(
+      my_circuit,
+      {target_ctx: {seq: []}},
+      # extensions: [], id: :Create
+      compiler: my_canonical_compiler,
+    )
+
+    assert_equal signal, Trailblazer::Activity::Right
+    assert_equal lib_ctx[:target_ctx][:seq], [:a]
   end
 end
